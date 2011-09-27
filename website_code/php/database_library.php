fef360409b76d9ccd691cfec9e5cc0fe69dd8233
Dave Goodwin <gingerdog@gmail.com>
2011-09-27 15:27:17 +0000

    there is no point in assigning a varialbe in a die; reindent
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@84 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- website_code/php/database_library.php
+++ website_code/php/database_library.php
@@ -1,71 +1,62 @@
-<?PHP     /**	
-	 * 
-	 * Database library, code for connecting to the database
-	 *
-	 * @author Patrick Lockley
-	 * @version 1.0
-	 * @copyright Copyright (c) 2008,2009 University of Nottingham
-	 * @package
-	 */
-	 
-	 /**
-	 * 
-	 * Function database connect
- 	 * This function checks http security settings
-	 * @param string $success_string = Successful message for the error log
- 	 * @param string $error_string = Error message for the error log
-	 * @version 1.0
-	 * @author Patrick Lockley
-	 */
+<?php
+
+/**	
+ * 
+ * Database library, code for connecting to the database
+ *
+ * @author Patrick Lockley
+ * @version 1.0
+ * @copyright Copyright (c) 2008,2009 University of Nottingham
+ * @package
+ */
+
+/**
+ * 
+ * Function database connect
+ * This function checks http security settings
+ * @param string $success_string = Successful message for the error log
+ * @param string $error_string = Error message for the error log
+ * @version 1.0
+ * @author Patrick Lockley
+ */
 
 function database_connect($success_string, $error_string){
-	
-	include_once("error_library.php");
 
-	global $xerte_toolkits_site;
-	
-	/*
-	* Try to connect
-	*/
+    include_once("error_library.php");
 
-	$mysql_connect_id = @mysql_connect($xerte_toolkits_site->database_host, $xerte_toolkits_site->database_username, $xerte_toolkits_site->database_password);
+    global $xerte_toolkits_site;
 
-	/*
-	* Check for connection and error if failed
-	*/
+    /*
+     * Try to connect
+     */
 
-	if(!$mysql_connect_id){
+    $mysql_connect_id = @mysql_connect($xerte_toolkits_site->database_host, $xerte_toolkits_site->database_username, $xerte_toolkits_site->database_password);
 
-		die("Sorry, the system cannot connect to the database at present. The mysql error is " . mysql_error() );
-		
-	}
-	
-	$database_fail = false;
+    /*
+     * Check for connection and error if failed
+     */
 
-	mysql_select_db($xerte_toolkits_site->database_name) or die($database_fail = true);
-	
-	/*
-	* database failing code
-	*/
+    if(!$mysql_connect_id){
+        die("Sorry, the system cannot connect to the database at present. The mysql error is " . mysql_error() );
+    }
 
-	if($database_fail){
+    $database_fail = false;
 
-		receive_message($_SESSION['toolkits_logon_username'], "ADMIN", "CRITICAL", "DATABASE FAILED AT " . $error_string, "MYSQL ERROR MESSAGE IS " . mysql_error());
-		die("Sorry, the system cannot connect to the database at present. The mysql error is " . mysql_error() );
+    mysql_select_db($xerte_toolkits_site->database_name) or ($database_fail = true);
 
+    /*
+     * database failing code
+     */
 
-	}else{
+    if($database_fail){
+        receive_message($_SESSION['toolkits_logon_username'], "ADMIN", "CRITICAL", "DATABASE FAILED AT " . $error_string, "MYSQL ERROR MESSAGE IS " . mysql_error());
+        die("Sorry, the system cannot connect to the database at present. The mysql error is " . mysql_error() );
+    }else{
+        receive_message($_SESSION['toolkits_logon_username'], "ADMIN", "SUCCESS", "DATABASE CONNECTED", $success_string);
+    }
 
-		receive_message($_SESSION['toolkits_logon_username'], "ADMIN", "SUCCESS", "DATABASE CONNECTED", $success_string);
-
-	}
-	
-	/*
-	* if all worked returned the mysql ID
-	*/
-
-	return $mysql_connect_id;
-
+    /*
+     * if all worked returned the mysql ID
+     */
+    return $mysql_connect_id;
 }
-
-?>
