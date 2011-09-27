843dc445659f5176dd89d3517664ce097c877c75
Dave Goodwin <gingerdog@gmail.com>
2011-09-27 15:48:34 +0000

    fix undefined (but required) global variable
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@85 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- modules/xerte/module_functions.php
+++ modules/xerte/module_functions.php
@@ -1,29 +1,28 @@
-<?PHP     /**
-* 
-* module functions page, shared functions for this module
-*
-* @author Patrick Lockley
-* @version 1.0
-* @copyright Copyright (c) 2008,2009 University of Nottingham
-* @package
-*/
+<?php
+/**
+ * 
+ * module functions page, shared functions for this module
+ *
+ * @author Patrick Lockley
+ * @version 1.0
+ * @copyright Copyright (c) 2008,2009 University of Nottingham
+ * @package
+ */
 
 
 /**
-* 
-* Function dont_show_template
-* This function outputs the HTML for people have no rights to this template
-* @version 1.0
-* @author Patrick Lockley
-*/
+ * 
+ * Function dont_show_template
+ * This function outputs the HTML for people have no rights to this template
+ * @version 1.0
+ * @author Patrick Lockley
+ */
 
 function dont_show_template(){
+    global $xerte_toolkits_site;
 
-	require("config.php");	
-
-	echo edit_xerte_page_format_top(file_get_contents($xerte_toolkits_site->website_code_path . "error_top")) . " Sorry, the author of this piece has yet to make it available.</div></div></body></html>";
-	die();
-
+    echo edit_xerte_page_format_top(
+        file_get_contents($xerte_toolkits_site->website_code_path . "error_top")) . 
+        " Sorry, the author of this piece has yet to make it available.</div></div></body></html>";
+    die();
 }
-
-?>
