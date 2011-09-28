035d4c69fd3c98bc716950cf16f52daf01438d2b
Dave Goodwin <gingerdog@gmail.com>
2011-09-28 14:39:40 +0000

    reident; not sure this is used as it does not look like it could/would work (at least if called directly)
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@102 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- drawing.php
+++ drawing.php
@@ -1,23 +1,27 @@
-<?PHP     /**
-	 * 
-	 * Drawing page, brings up the xerte drawing tool in another window
-	 *
-	 * @author Patrick Lockley
-	 * @version 1.0
-	 * @copyright Copyright (c) 2008,2009 University of Nottingham
-	 * @package
-	 */
+<?php
+/**
+ * 
+ * Drawing page, brings up the xerte drawing tool in another window
+ *
+ * @author Patrick Lockley
+ * @version 1.0
+ * @copyright Copyright (c) 2008,2009 University of Nottingham
+ * @package
+ */
 
-	echo file_get_contents("modules/xerte/drawing_xerte_top");
+require_once('config.php');
 
-	echo "so.addVariable(\"xmlvariable\", \"$string_for_flash_xml\");";
-	echo "so.addVariable(\"rlovariable\", \"$string_for_flash_media\");";
- 	echo "so.addVariable(\"originalpathvariable\", \"$string_for_flash_xwd\");";
- 	echo "so.addVariable(\"template_id\", \"" . $row['template_id'] . "\");";
 
-	echo "so.write(\"flashcontent\");";
-	echo "</script>";
+echo file_get_contents("modules/xerte/drawing_xerte_top");
 
-	echo "</body></html>";
+echo "so.addVariable(\"xmlvariable\", \"$string_for_flash_xml\");";
+echo "so.addVariable(\"rlovariable\", \"$string_for_flash_media\");";
+echo "so.addVariable(\"originalpathvariable\", \"$string_for_flash_xwd\");";
+echo "so.addVariable(\"template_id\", \"" . $row['template_id'] . "\");";
+
+echo "so.write(\"flashcontent\");";
+echo "</script>";
+
+echo "</body></html>";
 
 ?>
