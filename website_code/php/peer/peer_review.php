ff13d0ac4b6c24b4354512ab604965130f5e376d
Dave Goodwin <gingerdog@gmail.com>
2011-10-05 21:09:24 +0000

    reindent; remove mysql_* stuff; replace with db_query; ensure vaguely valid form submission
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@130 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- website_code/php/peer/peer_review.php
+++ website_code/php/peer/peer_review.php
@@ -1,39 +1,32 @@
-<?PHP     /**
-	 * 
-	 * peer view page, sends the email back to the 
-	 *
-	 * @author Patrick Lockley
-	 * @version 1.0
-	 * @copyright Copyright (c) 2008,2009 University of Nottingham
-	 * @package
-	 */
-	
-	require("../../../config.php");
+<?php 
+/**
+ * 
+ * peer view page, sends the email back to the 
+ *
+ * @author Patrick Lockley
+ * @version 1.0
+ * @copyright Copyright (c) 2008,2009 University of Nottingham
+ * @package
+ */
 
-	include "../database_library.php";
+require_once("../../../config.php");
 
-	$mysql_id = database_connect("peer review Database connect success","peer review database connect failed");
+include "../database_library.php";
 
-	$query_for_file_name = "select template_name from " . $xerte_toolkits_site->database_table_prefix . "templatedetails where template_id =\"" . mysql_real_escape_string($_POST['template_id']) . "\"";
+if(empty($_POST['template_id'])) {
+    die("invalid form submission");
+}
 
-	$query_response = mysql_query($query_for_file_name);
+$query_for_file_name = "select template_name from {$xerte_toolkits_site->database_table_prefix}templatedetails where template_id =?";
 
-	$row_template_name = mysql_fetch_array($query_response);
+$row_template_name = db_query_one($query_for_file_name, array($_POST['template_id']));
 
-	$headers = str_replace("*","\n",$xerte_toolkits_site->headers);
+$headers = str_replace("*","\n",$xerte_toolkits_site->headers);
 
-	if(isset($_POST['user'])){
-
-		if(mail( $_POST['user'] . "@" . $xerte_toolkits_site->email_to_add_to_username, "Feedback on project - \"" . str_replace("_"," ",$row_template_name['template_name']) ."\"", "Hello, <br><br> You've received feedback on your project.<br><br><br>" . $_POST['feedback'] . "<br><br><br>Thanks for using the site<br><br>The Xerte Project Team", $headers)){
-
-			echo "<b>Your feedback has been sent to the user</b>";
-
-		}else{
-
-			echo "<b>A problem has occured.</b>";
-
-		}
-
-	}
-
-?>
+if(isset($_POST['user'])){
+    if(mail( $_POST['user'] . "@" . $xerte_toolkits_site->email_to_add_to_username, "Feedback on project - \"" . str_replace("_"," ",$row_template_name['template_name']) ."\"", "Hello, <br><br> You've received feedback on your project.<br><br><br>" . $_POST['feedback'] . "<br><br><br>Thanks for using the site<br><br>The Xerte Project Team", $headers)){
+        echo "<b>Your feedback has been sent to the user</b>";
+    }else{
+        echo "<b>A problem has occured.</b>";
+    }
+}
