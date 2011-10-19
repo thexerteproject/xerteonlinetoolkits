56470f794fa74271a62b4902bca6ebd418422815
Dave Goodwin <gingerdog@gmail.com>
2011-10-19 10:59:07 +0000

    reindent; require_once etc
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/branches/1.8@146 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- website_code/php/import/fileupload.php
+++ website_code/php/import/fileupload.php
@@ -1,56 +1,56 @@
 <?PHP
 
-	include "../../../config.php";
+require_once "../../../config.php";
 
-	_load_language_file("/website_code/php/fileupload.inc");
+_load_language_file("/website_code/php/fileupload.inc");
 
-	if(in_array($_FILES['filenameuploaded']['type'],$xerte_toolkits_site->mimetypes)){
+if(in_array($_FILES['filenameuploaded']['type'],$xerte_toolkits_site->mimetypes)){
 
-		if($_FILES['filenameuploaded']['type']=="text/html"){
+    if($_FILES['filenameuploaded']['type']=="text/html"){
 
-			$php_check = file_get_contents($_FILES['filenameuploaded']['tmp_name']);
-	
-			if(!strpos($php_check,"<?PHP")){
+        $php_check = file_get_contents($_FILES['filenameuploaded']['tmp_name']);
 
-				$new_file_name = $_POST['mediapath'] . $_FILES['filenameuploaded']['name'];
+        if(!strpos($php_check,"<?PHP")){
 
-				if(@move_uploaded_file($_FILES['filenameuploaded']['tmp_name'], $new_file_name)){
+            $new_file_name = $_POST['mediapath'] . $_FILES['filenameuploaded']['name'];
 
-					echo FILE_UPLOAD_SUCCESS . "****";
+            if(@move_uploaded_file($_FILES['filenameuploaded']['tmp_name'], $new_file_name)){
 
-				}else{
+                echo FILE_UPLOAD_SUCCESS . "****";
 
-					echo FILE_UPLOAD_ZIP_FAIL . "****";
+            }else{
 
-				}
+                echo FILE_UPLOAD_ZIP_FAIL . "****";
 
-			}else{
+            }
 
-				echo FILE_UPLOAD_HTML_FAIL . "****";				
+        }else{
 
-			}
+            echo FILE_UPLOAD_HTML_FAIL . "****";				
 
-		}else{
+        }
 
-			$new_file_name = $_POST['mediapath'] . $_FILES['filenameuploaded']['name'];
+    }else{
 
-			if(@move_uploaded_file($_FILES['filenameuploaded']['tmp_name'], $new_file_name)){
+        $new_file_name = $_POST['mediapath'] . $_FILES['filenameuploaded']['name'];
 
-				echo FILE_UPLOAD_SUCCESS . "****";
+        if(@move_uploaded_file($_FILES['filenameuploaded']['tmp_name'], $new_file_name)){
 
-			}else{
+            echo FILE_UPLOAD_SUCCESS . "****";
 
-				echo FILE_UPLOAD_ZIP_FAIL . "****";
+        }else{
 
-			}
+            echo FILE_UPLOAD_ZIP_FAIL . "****";
 
-		}
+        }
+
+    }
 
 
-	}else{
+}else{
 
-		echo FILE_UPLOAD_MIME_FAIL . " - " . $_FILES['filenameuploaded']['type'] . "****";
+    echo FILE_UPLOAD_MIME_FAIL . " - " . $_FILES['filenameuploaded']['type'] . "****";
 
-	}
+}
 
 ?>
