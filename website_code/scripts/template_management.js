f8d0d5ac673d33cf98cc53e9c59b87249175fb26
Dave Goodwin <gingerdog@gmail.com>
2011-10-21 09:29:10 +0000

    perhaps stop IE7 moaning
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@177 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- website_code/scripts/template_management.js
+++ website_code/scripts/template_management.js
@@ -1176,8 +1176,8 @@ function tutorial_created(){
 
 	if (xmlHttp.readyState==4){ 
         if( typeof xmlHttp.responseText == 'string' ) {
-            response = String(response);
-            response = xmlHttp.responseText.trim();
+            response = String(xmlHttp.responseText);
+            response = response.trim();
             if(response!=""){
                 var neweditorwindow = window.open(site_url + url_return("edit" , xmlHttp.responseText.split(" ").join("")), "editwindow" + xmlHttp.responseText.split(" ").join(""), "height=665, width=800" );
                 new_file = xmlHttp.responseText;
