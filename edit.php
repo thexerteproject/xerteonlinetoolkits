ba349338097b663718b440eb92bc5a5cf10b2b97
Dave Goodwin <gingerdog@gmail.com>
2011-10-21 12:20:32 +0000

    this was missing - only relevant in error-ish conditions
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@184 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- edit.php
+++ edit.php
@@ -16,6 +16,8 @@ require $xerte_toolkits_site->php_library_path . "template_status.php";
 require $xerte_toolkits_site->php_library_path . "display_library.php";
 require $xerte_toolkits_site->php_library_path . "user_library.php";
 
+require_once dirname(__FILE__) . '/modules/xerte/module_functions.php';
+
 /**
  * 
  * Function update_access_time
