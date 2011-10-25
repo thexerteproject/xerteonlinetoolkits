bcca97dc8e06df45b12a209220c54097cc08bbc0
Dave Goodwin <gingerdog@gmail.com>
2011-10-25 17:52:35 +0000

    remove unnecessary config file inclusions; reindent etc (merge of changes in trunk)
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/branches/1.8@192 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- modules/xerte/peer.php
+++ modules/xerte/peer.php
@@ -1,72 +1,70 @@
-<?PHP     /**
-* 
-* peer page, allows the site to make a peer review page for a xerte module
-*
-* @author Patrick Lockley
-* @version 1.0
-* @copyright Copyright (c) 2008,2009 University of Nottingham
-* @package
-*/
+<?php 
+/**
+ * 
+ * peer page, allows the site to make a peer review page for a xerte module
+ *
+ * @author Patrick Lockley
+ * @version 1.0
+ * @copyright Copyright (c) 2008,2009 University of Nottingham
+ * @package
+ */
 
 /**
-* 
-* Function show template
-* This function creates folders needed when creating a template
-* @param array $row_play - an array from the last mysql query
-* @version 1.0
-* @author Patrick Lockley
-*/
+ * 
+ * Function show template
+ * This function creates folders needed when creating a template
+ * @param array $row_play - an array from the last mysql query
+ * @version 1.0
+ * @author Patrick Lockley
+ */
 
 function show_template($row_play){
+    global $xerte_toolkits_site;
 
-		require("config.php");
+    _load_language_file("/modules/xerte/peer.inc");
 
-		_load_language_file("/modules/xerte/peer.inc");
+    $string_for_flash_xml = $xerte_toolkits_site->users_file_area_short . $row_play['template_id'] . "-" . $row_play['username'] . "-" . $row_play['template_name'] . "/data.xml";
 
-		$string_for_flash_xml = $xerte_toolkits_site->users_file_area_short . $row_play['template_id'] . "-" . $row_play['username'] . "-" . $row_play['template_name'] . "/data.xml";
+    $string_for_flash = $xerte_toolkits_site->users_file_area_short . $row_play['template_id'] . "-" . $row_play['username'] . "-" . $row_play['template_name'] . "/";
 
-		$string_for_flash = $xerte_toolkits_site->users_file_area_short . $row_play['template_id'] . "-" . $row_play['username'] . "-" . $row_play['template_name'] . "/";
-
-		list($x, $y) = explode("~",get_template_screen_size($row_play['template_name'],$row_play['template_framework']));
-
-		?>
-		<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html lang="en">
-		<head>
-		<title>Interactive Learning Content</title>
-		<script src = "rloObject.js"></script>
-		<script type="text/javascript" language="Javascript" src="website_code/scripts/peer.js"></script>
-		<script type="text/javascript" language="Javascript" src="website_code/scripts/ajax_management.js"></script>
-		<script type="text/javascript">
-		function enableTTS(){
-		  if (navigator.appName.indexOf("Microsoft") != -1){
-			VoiceObj = new ActiveXObject("Sapi.SpVoice");
-		  }
-		}
-		</script>
-		</head>
-		<body style="#ffffff; font-family:verdana,tahoma,arial; font-size:80%;">
-		<div style="width:900px; margin:0 auto;">
-		<p style="margin 0px; padding:0px">
-		<b><?PHP echo XERTE_PEER_DESCRIPTION; ?></b><br><?PHP echo XERTE_PEER_GUIDANCE;?> 
-		</p>
-		<script type="text/javascript" language="JavaScript">
-		
-		<?PHP
-
-		/*
-		* slightly modified xerte preview code to allow for flash vars
-		*/
-
-		echo "myRLO = new rloObject('" . $x . "','" . $y . "','modules/" . $row_play['template_framework'] . "/parent_templates/" . $row_play['template_name'] . "/" . $row_play['template_name'] . ".rlt','$string_for_flash', '$string_for_flash_xml', '$xerte_toolkits_site->site_url')";
-
-		echo "</script>";
-		
-		echo "<a name=\"feedbackform\"><p style=\"width:250px; color:red;\"  id=\"feedback\"></p></a>";
-
-		echo "<br><form name=\"peer\" action=\"javascript:send_review('" . $row_play['username'] . "','" . $row_play['template_id'] . "')\" method=\"post\" enctype=\"text/plain\"><textarea style=\"width:800px; height:300px;\" name=\"response\">You have been asked to provide some feedback on this learning object. Please enter your feedback and click save when you have finished. This feedback is anonymous.</textarea><br/><input type=\"image\" src=\"website_code/images/Bttn_SaveOff.gif\" onmouseover=\"this.src='website_code/images/Bttn_SaveOn.gif'\" onmousedown=\"this.src='website_code/images/Bttn_SaveClick.gif'\" onmouseout=\"this.src='website_code/images/Bttn_SaveOff.gif'\" /></form><a name=\"feedbackform\"><p style=\"width:250px;\"  id=\"feedback\"></p></a></div>";
-
-		echo "</body></html>";
-
-}
+    list($x, $y) = explode("~",get_template_screen_size($row_play['template_name'],$row_play['template_framework']));
 
 ?>
+        <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html lang="en">
+        <head>
+        <title>Interactive Learning Content</title>
+        <script src = "rloObject.js"></script>
+        <script type="text/javascript" language="Javascript" src="website_code/scripts/peer.js"></script>
+        <script type="text/javascript" language="Javascript" src="website_code/scripts/ajax_management.js"></script>
+    <script type="text/javascript">
+    function enableTTS(){
+        if (navigator.appName.indexOf("Microsoft") != -1){
+            VoiceObj = new ActiveXObject("Sapi.SpVoice");
+        }
+    }
+    </script>
+        </head>
+        <body style="#ffffff; font-family:verdana,tahoma,arial; font-size:80%;">
+        <div style="width:900px; margin:0 auto;">
+        <p style="margin 0px; padding:0px">
+        <b><?PHP echo XERTE_PEER_DESCRIPTION; ?></b><br><?PHP echo XERTE_PEER_GUIDANCE;?> 
+        </p>
+    <script type="text/javascript" language="JavaScript">
+
+<?PHP
+
+    /*
+     * slightly modified xerte preview code to allow for flash vars
+     */
+
+    echo "myRLO = new rloObject('" . $x . "','" . $y . "','modules/" . $row_play['template_framework'] . "/parent_templates/" . $row_play['template_name'] . "/" . $row_play['template_name'] . ".rlt','$string_for_flash', '$string_for_flash_xml', '$xerte_toolkits_site->site_url')";
+
+    echo "</script>";
+
+    echo "<a name=\"feedbackform\"><p style=\"width:250px; color:red;\"  id=\"feedback\"></p></a>";
+
+    echo "<br><form name=\"peer\" action=\"javascript:send_review('" . $row_play['username'] . "','" . $row_play['template_id'] . "')\" method=\"post\" enctype=\"text/plain\"><textarea style=\"width:800px; height:300px;\" name=\"response\">You have been asked to provide some feedback on this learning object. Please enter your feedback and click save when you have finished. This feedback is anonymous.</textarea><br/><input type=\"image\" src=\"website_code/images/Bttn_SaveOff.gif\" onmouseover=\"this.src='website_code/images/Bttn_SaveOn.gif'\" onmousedown=\"this.src='website_code/images/Bttn_SaveClick.gif'\" onmouseout=\"this.src='website_code/images/Bttn_SaveOff.gif'\" /></form><a name=\"feedbackform\"><p style=\"width:250px;\"  id=\"feedback\"></p></a></div>";
+
+    echo "</body></html>";
+
+}
