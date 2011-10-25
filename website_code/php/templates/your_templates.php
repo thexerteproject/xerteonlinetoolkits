433db09466959da78ffdb67c084ab586845a2257
Dave Goodwin <gingerdog@gmail.com>
2011-10-25 18:03:13 +0000

    merges from trunk (up to r163)
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/branches/1.8@193 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- website_code/php/templates/your_templates.php
+++ website_code/php/templates/your_templates.php
@@ -11,12 +11,6 @@ include "../display_library.php";
 
 include "../user_library.php";
 
-$database_connect_id = database_connect("your templates database connect success", "your templates database connect failed");
-
 $_SESSION['sort_type'] = "date_down";
 
 list_users_projects($_SESSION['sort_type']);		
-
-mysql_close($database_connect_id);
-
-?>
