7cc9647cf1883119c495a7f7dc3482d19544983f
Dave Goodwin <gingerdog@gmail.com>
2011-10-27 09:53:42 +0000

    reindent
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@203 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- modules/xerte/play.php
+++ modules/xerte/play.php
@@ -9,18 +9,18 @@ require_once("module_functions.php");
 
 function show_template($row_play){
 
-        global $xerte_toolkits_site;
+    global $xerte_toolkits_site;
 
-		$string_for_flash_xml = $xerte_toolkits_site->users_file_area_short . $row_play['template_id'] . "-" . $row_play['username'] . "-" . $row_play['template_name'] . "/data.xml?time=" . time();
+    $string_for_flash_xml = $xerte_toolkits_site->users_file_area_short . $row_play['template_id'] . "-" . $row_play['username'] . "-" . $row_play['template_name'] . "/data.xml?time=" . time();
 
-		$string_for_flash = $xerte_toolkits_site->users_file_area_short . $row_play['template_id'] . "-" . $row_play['username'] . "-" . $row_play['template_name'] . "/";
+    $string_for_flash = $xerte_toolkits_site->users_file_area_short . $row_play['template_id'] . "-" . $row_play['username'] . "-" . $row_play['template_name'] . "/";
 
-		list($x, $y) = explode("~",get_template_screen_size($row_play['template_name'],$row_play['template_framework']));
-		echo file_get_contents("modules/" . $row_play['template_framework'] . "/preview_" . $row_play['template_framework'] . "_top");
+    list($x, $y) = explode("~",get_template_screen_size($row_play['template_name'],$row_play['template_framework']));
+    echo file_get_contents("modules/" . $row_play['template_framework'] . "/preview_" . $row_play['template_framework'] . "_top");
 
-		// slightly modified xerte preview code to allow for flash vars
-		echo "myRLO = new rloObject('" . $x . "','" . $y . "','modules/" . $row_play['template_framework'] . "/parent_templates/" . $row_play['template_name'] . "/" . $row_play['template_name'] . ".rlt','$string_for_flash', '$string_for_flash_xml', '$xerte_toolkits_site->site_url')";
+    // slightly modified xerte preview code to allow for flash vars
+    echo "myRLO = new rloObject('" . $x . "','" . $y . "','modules/" . $row_play['template_framework'] . "/parent_templates/" . $row_play['template_name'] . "/" . $row_play['template_name'] . ".rlt','$string_for_flash', '$string_for_flash_xml', '$xerte_toolkits_site->site_url')";
 
-		echo "</script></div><div id=\"popup_parent\"></body></html>";
+    echo "</script></div><div id=\"popup_parent\"></body></html>";
 
 }
