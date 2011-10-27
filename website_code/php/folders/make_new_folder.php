fa0c8fbf0676c5d608650f06bec7f26b1fcd459a
Dave Goodwin <gingerdog@gmail.com>
2011-10-27 10:07:56 +0000

    reindent, re-order includes
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@206 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- website_code/php/folders/make_new_folder.php
+++ website_code/php/folders/make_new_folder.php
@@ -1,16 +1,15 @@
-<?PHP     /**
-	 * 
-	 * new folder page, the sites makes a new folder
-	 *
-	 * @author Patrick Lockley
-	 * @version 1.0
-	 * @copyright Copyright (c) 2008,2009 University of Nottingham
-	 * @package
-	 */
+<?php
+/**
+ * 
+ * new folder page, the sites makes a new folder
+ *
+ * @author Patrick Lockley
+ * @version 1.0
+ * @copyright Copyright (c) 2008,2009 University of Nottingham
+ * @package
+ */
 
-	include '../folder_library.php';
-	require_once("../../../config.php");
-	
-	make_new_folder(mysql_real_escape_string($_POST['folder_id']),mysql_real_escape_string($_POST['folder_name']));
+require_once("../../../config.php");
+include '../folder_library.php';
 
-?>
+make_new_folder(mysql_real_escape_string($_POST['folder_id']),mysql_real_escape_string($_POST['folder_name']));
