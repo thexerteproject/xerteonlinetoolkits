f8c5054c8b9356b05f79d291a174c9507f55cb85
Tom Reijnders <ajjreijnders@gmail.com>
2012-10-12 11:20:24 +0000

    Fix xml error on export with non-escaped '&'
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@451 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- website_code/php/xmlInspector.php
+++ website_code/php/xmlInspector.php
@@ -20,10 +20,45 @@ class XerteXMLInspector
     array_push($this->models, $model);
   }
 
+  private function fixXmlFile($name)
+  {
+    $xmlcontents = file_get_contents($name);
+    //_debug("1. " . $xmlcontents);
+    // Ok replace known escape sequences to something without an '&'
+    $xmlcontents = str_replace('&lt;', '%%%lt;', $xmlcontents);
+    $xmlcontents = str_replace('&gt;', '%%%gt;', $xmlcontents);
+    $xmlcontents = str_replace('&quot;', '%%%quot;', $xmlcontents);
+    $xmlcontents = str_replace('&nbsp;', '%%%nbsp;', $xmlcontents);
+    $xmlcontents = str_replace('&amp;', '%%%amp;', $xmlcontents);
+
+    //_debug("2. " . $xmlcontents);
+    // replace & with &amp;
+    $xmlcontents = str_replace('&', '&amp;', $xmlcontents);
+
+    // replace known escape sequences back
+    $xmlcontents = str_replace('%%%lt;', '&lt;', $xmlcontents);
+    $xmlcontents = str_replace('%%%gt;', '&gt;', $xmlcontents);
+    $xmlcontents = str_replace('%%%quot;', '&quot;', $xmlcontents);
+    $xmlcontents = str_replace('%%%nbsp;', '&nbsp;', $xmlcontents);
+    $xmlcontents = str_replace('%%%amp;', '&amp;', $xmlcontents);
+
+    //_debug("3. ". $xmlcontents);
+
+    $res = file_put_contents($name, $xmlcontents);
+    //_debug("4. " . $res);
+  }
+
   public function loadTemplateXML($name)
   {
     $this->fname = $name;
+    libxml_use_internal_errors(true);
     $this->xml = simplexml_load_file($name);
+    if (!$this->xml)
+    {
+        _debug("Error detected in XML, try to fix...");
+        $this->fixXmlFile($name);
+        $this->xml = simplexml_load_file($name);
+    }
     $this->models = array();
     $nodes = $this->xml->xpath('/*/*');
     foreach ($nodes as $node)
