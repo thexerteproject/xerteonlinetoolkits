1b543db1deb30d6d4171dd398db67d96183eda9f
Patrick Lockley <patrick.lockley@googlemail.com>
2012-11-22 23:13:03 +0000

    Modularised
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@540 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- modules/xerte/export.php
+++ modules/xerte/export.php
@@ -1,68 +1,11 @@
-<?php 
-require_once(dirname(__FILE__) . '/../../config.php');
-/**
- * 
- * module functions page, shared functions for this module
- *
- * @author Patrick Lockley
- * @version 1.0
- * @copyright Copyright (c) 2008,2009 University of Nottingham
- * @package
- */
+<?
 
-require_once(dirname(__FILE__) . '/../../config.php');
-
-/**
- * 
- * Function dont_show_template
- * This function outputs the HTML for people have no rights to this template
- * @version 1.0
- * @author Patrick Lockley
- */
-
-function dont_show_template($optional=''){
-
-    _load_language_file("/modules/xerte/module_functions.inc");
-
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-    <script src="modules/Xerte/javascript/swfobject.js"></script>
-    <script src="website_code/scripts/opencloseedit.js"></script>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-    <link href="website_code/styles/frontpage.css" media="screen" type="text/css" rel="stylesheet" />
-    </head>
-
-    <body>
-
-    <div style="margin:0 auto; width:800px">
-    <div class="edit_topbar" style="width:800px">
-        <img src="website_code/images/edit_xerteLogo.jpg" style="margin-left:10px; float:left" />
-        <img src="website_code/images/edit_UofNLogo.jpg" style="margin-right:10px; float:right" />
-    </div>	
-    <div style="margin:0 auto">
-<?PHP
-
-    echo XERTE_DISPLAY_FAIL;
-if($optional!=='') {
-      echo '</div><div>' . $optional;
-}
-    ?></div></div></body></html><?PHP
-
-        die();
-
-}
-
-$folder_id_array = array();
-$folder_array = array();
-$file_array = array();
-$delete_file_array = array();
-$delete_folder_array = array();
-$zipfile = "";
-
-function export_template($row, $fullArchive){
-
-	global $xerte_toolkits_site, $folder_id_array, $folder_array, $file_array, $delete_file_array, $delete_folder_array, $zipfile;
+	$folder_id_array = array();
+	$folder_array = array();
+	$file_array = array();
+	$delete_file_array = array();
+	$delete_folder_array = array();
+	$zipfile = "";
 
 	include "archive.php";
 	include "scorm_library.php";
@@ -242,7 +185,5 @@ function export_template($row, $fullArchive){
 	 */
 	clean_up_files();
 	unlink($dir_path . "template.xml");
-
-}
 
 ?>
