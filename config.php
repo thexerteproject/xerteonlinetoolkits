a9d313aad3f878e79e7b6aa0cbb124cc09c5e362
ronm123 <ronm123@git.hidden>
2012-11-26 20:16:03 +0000

    extracted the commented authentication options from config.php and added to a required file named auth_config.php. This means when updating/upgrading config.php can be overwritten without changing the authentication method back to Guest. When updating/upgrading auth_config.php should be excluded or re-edited after update. Updated the moodle_integration_readme.txt instructions to reflect this change.
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@553 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- config.php
+++ config.php
@@ -171,47 +171,5 @@ if (!isset($xerte_toolkits_site)) {
       array($session_handle,'xerte_session_destroy'),
       array($session_handle,'xerte_session_clean'));
      */
-/**
- *Change this to reflect the authentication mechanism you wish to use. 
- * Possible values: Ldap, Db, Static and Guest. (perhaps soon also Moodle).
- *Guest will let anyone login as the same user, once they click 'Login'. 
- *
- * See code in library/Xerte/Authentication/*.php - where each file should match up to the value used below.
- */
-    
-    $xerte_toolkits_site->authentication_method = 'Guest';
-    //$xerte_toolkits_site->authentication_method = 'Ldap';
-    //$xerte_toolkits_site->authentication_method = 'Db';
-    //$xerte_toolkits_site->authentication_method = 'Static';
-    //$xerte_toolkits_site->authentication_method = "Moodle";
-    
-    //restrict moodle guest access
-    //comment out the following if you want the Moodle guest account to have authoring access
-   if ( $xerte_toolkits_site->authentication_method=="Moodle"){
-    if($USER->username=='guest'){
-    echo '<p style="text-align:center; font-family:verdana;"><br></br></font>Sorry you do not currently have permission to author with Xerte.</p>';
-exit;
-    }}
-    
-//restrict moodle access via custom moodle profile field named xot
-//in moodle set it to be a checkbox and either checked or unchecked by default
-//then either check or uncheck for those who should have XOT authoring access
-//change the require path below to point to your moodle directory/user/profile/lib.php
-//require_once('/moodle/user/profile/lib.php'); 
-//profile_load_data($USER);
-//if ($USER->profile_field_xot!='1'){
-//echo '<p style="text-align:center; font-family:verdana;"><br></br></font>Sorry you do not currently have permission to author with Xerte.</p>';
-//exit;
-//}else{
-//echo 'yep you are ok';
-//}
-//}
-    
-
-    if($xerte_toolkits_site->authentication_method == "Moodle") {
-        // skip session_start() as we'll probably stomp on Moodle's session if we do. 
-    }
-    else {
-        session_start();
-    }
+require_once(dirname(__FILE__) . '/auth_config.php');
 }
