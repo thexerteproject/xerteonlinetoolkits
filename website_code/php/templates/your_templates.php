faf5685a98f925046bb17059b78fe75fd08bbfec
ronm123 <ronm123@git.hidden>
2012-12-05 16:00:50 +0000

    Pat found and removed a duplicate require path. May not have been causing a problem unless Moodle integration used but the same file shouldn't be required twice in the same page anyway - at least not one line below the other.
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@577 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- website_code/php/templates/your_templates.php
+++ website_code/php/templates/your_templates.php
@@ -9,9 +9,11 @@ require_once("../../../config.php");
 
 require_once("../display_library.php");
 
-require_once("../display_library.php");
+database_connect("your template connect","your template fail");
+
 require_once("../user_library.php");
 
 $_SESSION['sort_type'] = "date_down";
 
 list_users_projects($_SESSION['sort_type']);		
+
