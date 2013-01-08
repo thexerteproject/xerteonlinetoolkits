9fa26201a37138fd9a47e1de50dd709025bcfa07
Julian Tenney <julian.tenney@nottingham.ac.uk>
2013-01-08 15:32:14 +0000

    Attempt at security fix. Needs more work to make this more robust.
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@618 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- modules/xerte/engine/upload.php
+++ modules/xerte/engine/upload.php
@@ -11,12 +11,27 @@
 
 require_once("../../../config.php");
 
+if(!isset($_SESSION['toolkits_logon_username'])) {
+  print "You are not logged in.";
+  exit();
+}
+
 $page_sought = explode("=",$_SERVER['REQUEST_URI']);
+
+// SECURITY / TODO / XXX - someone can use this to upload an arbitrary file to a place of their choosing on the server 
+$pass = true;
+if (strpos($_FILES['Filedata']['name'], '../') !== false) $pass = false;
+if (strpos($_FILES['Filedata']['name'], '.exe') !== false) $pass = false;
+if (strpos($_FILES['Filedata']['name'], '...') !== false) $pass = false;
+
+if ($pass === false){
+  print "Invalid File Name";
+  exit();
+}
+
 
 $new_file_name = $xerte_toolkits_site->root_file_path . $page_sought[1] . $_FILES['Filedata']['name'];
 
-// SECURITY / TODO / XXX - someone can use this to upload an arbitrary file to a place of their choosing on the server 
-// (assuming it's writeable); $_FILES['x']['name'] can contain ../../ as it's user supplied.
 if(move_uploaded_file($_FILES['Filedata']['tmp_name'], $new_file_name)){
 }else{
 }
