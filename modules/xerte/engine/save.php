9ed0f56cc81e03674ab5d0debd23c3f8ec0ae023
Julian Tenney <julian.tenney@nottingham.ac.uk>
2013-01-08 15:32:14 +0000

    Attempt at security fix. Needs more work to make this more robust.
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@618 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- modules/xerte/engine/save.php
+++ modules/xerte/engine/save.php
@@ -11,6 +11,11 @@
 
 require_once("../../../config.php");
 
+if(!isset($_SESSION['toolkits_logon_username'])) {
+  print "You are not logged in.";
+  exit();
+}
+
 $savepath = str_replace("preview.xml","data.xml",$_POST['filename']);
 
 /**
