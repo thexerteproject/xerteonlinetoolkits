60bf4e9dbe2ffb342624d327e858cddc6022c4f0
Julian Tenney <julian.tenney@nottingham.ac.uk>
2013-02-11 11:48:21 +0000

    Tidying up the main application logic
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@678 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- modules/xerte/parent_templates/bootstrap/common/js/application.js
+++ modules/xerte/parent_templates/bootstrap/common/js/application.js
@@ -1,9 +1,11 @@
 
 $(document).ready(init);
 
+var data;
+
 function init()
 {	
-	loadContent(0);
+	loadContent();
 };
 
 function initMedia()
@@ -27,112 +29,126 @@ function initSidebar(){
 	)
 }
 
-function loadContent(index){
+function loadContent(){
 
 	$.ajax({
 	
 		type: "GET",
 		url: projectXML,
 		dataType: "xml", 
-		success: function(data) {
+		success: function(xml) {
 		
-		if (typeof data == 'string'){
-			//in IE we need to turn the string into xml
-			data = $.parseXML(data);
-		}
-
-		//var FileLocation = '';
-		
-			//clear out existing content
-			$('#mainContent').empty();
-			$('#toc').empty();
-			$('#nav').empty();
+			if (typeof data == 'string'){
 			
-			//set the header image, if defined
-			if ($(data).find('learningObject').attr('header') != undefined){
-				$('#overview').css('background-image', "url('" + eval( $(data).find('learningObject').attr('header'))+ "')");
+				//in IE we need to turn the string into xml
+				data = $.parseXML(xml);
+				
+			} else {
+			
+				data = xml;
+				
 			}
 			
 			//add all the pages to the pages menu: this links bak to the same page
 			$(data).find('page').each( function(index, value){
-				$('#nav').append('<li class=""><a href="javascript:loadContent(' + index + ')">' + $(this).attr('name') + '</a></li>');
-			});
-		
-			//which page is this from the document?
-			var page = $(data).find('page').eq(index);
 			
-			//set the main page title and subtitle			
-			$('#pageTitle').text( page.attr('name') );
-			$('#pageSubTitle').text( page.attr('subtitle') );
-			
-			//create the sections
-			
-			//page.find('section').each( function(index, value){
-			page.find("section").each( function(index, value){
-			
-						
-				//add a TOC entry
-				$('#toc').append('<li><a href="#section' + index + '">' + $(this).attr('name') + '</a></li>');
+				$('#nav').append('<li class=""><a href="javascript:parseContent(' + index + ')">' + $(this).attr('name') + '</a></li>');
 				
-				//add the section header
-				var section = $('<section id="section' + index + '"><div class="page-header"><h1>' + $(this).attr('name') + '</h1></div></section>');
-
-				//add the section contents
-				$(this).children().each( function(index, value){
-					
-					//for all nodes append the text
-					section.append( '<p>' + $(this).text() + '</p>');
-					
-					//handle media
-					if ( $(this).attr('url') != undefined){
-					
-						var mediaStr;
-						var file = eval( $(this).attr('url') );
-						
-						if (this.nodeName == 'image'){
-						
-							//alert(file);
-							mediaStr = '<p><img class="img-polaroid" src="' + file + '" title="Alt Text" alt="Alt Text"/></p>';
-							
-						}
-						
-						if (this.nodeName == 'audio'){
-						
-							mediaStr = '<p><b>' + $(this).attr('name') + '</b></p><p><audio src="' + file + '" type="video/mp4" id="player1" controls="controls" preload="none" width="100%"></audio></p>';
-						
-						}
-						
-						if (this.nodeName == 'video'){
-						
-							mediaStr = '<p><b>' + $(this).attr('name') + '</b></p><p><video style="max-width: 100%" class="fullPageVideo" src="' + file + '" type="video/mp4" id="player1" controls="controls" preload="none"></video></p>';
-						
-						}
-						
-						//now append it to the section
-						section.append(mediaStr);
-					}
-				});
-				
-				//a return to top button
-				section.append( $('<p><br><a class="btn btn-mini pull-right" href="#">Top</a></p>'));
-
-				//add the section to the document
-				$('#mainContent').append(section);
-							
 			});
 			
-			//finish initialising the piece now we have the content loaded
-			initMedia();
-			initSidebar();
+			//set the header image, if defined
+			if ($(data).find('learningObject').attr('header') != undefined){
 			
-			window.scroll(0,0);
+				$('#overview').css('background-image', "url('" + eval( $(data).find('learningObject').attr('header'))+ "')");
+				
+			}
 			
-			$('body').scrollspy('refresh');
-
-			//force facebook / twitter objects to initialise
-			twttr.widgets.load();
-			FB.XFBML.parse(); 
-	
+			//done with one time stuff, now parse the first page
+			parseContent(0);
 		}
-	})
+		
+	});
+	
+}
+
+function parseContent(index){
+
+	//clear out existing content
+	$('#mainContent').empty();
+	$('#toc').empty();
+
+	//which page is this from the document?
+	var page = $(data).find('page').eq(index);
+	
+	//set the main page title and subtitle			
+	$('#pageTitle').text( page.attr('name') );
+	$('#pageSubTitle').text( page.attr('subtitle') );
+	
+	
+	//create the sections
+	page.find('section').each( function(index, value){
+	
+				
+		//add a TOC entry
+		$('#toc').append('<li><a href="#section' + index + '">' + $(this).attr('name') + '</a></li>');
+		
+		//add the section header
+		var section = $('<section id="section' + index + '"><div class="page-header"><h1>' + $(this).attr('name') + '</h1></div></section>');
+
+		//add the section contents
+		$(this).children().each( function(index, value){
+			
+			//for all nodes append the text
+			section.append( '<p>' + $(this).text() + '</p>');
+			
+			//handle media
+			if ( $(this).attr('url') != undefined){
+			
+				var mediaStr;
+				var file = eval( $(this).attr('url') );
+				
+				if (this.nodeName == 'image'){
+				
+					//alert(file);
+					mediaStr = '<p><img class="img-polaroid" src="' + file + '" title="Alt Text" alt="Alt Text"/></p>';
+					
+				}
+				
+				if (this.nodeName == 'audio'){
+				
+					mediaStr = '<p><b>' + $(this).attr('name') + '</b></p><p><audio src="' + file + '" type="video/mp4" id="player1" controls="controls" preload="none" width="100%"></audio></p>';
+				
+				}
+				
+				if (this.nodeName == 'video'){
+				
+					mediaStr = '<p><b>' + $(this).attr('name') + '</b></p><p><video style="max-width: 100%" class="fullPageVideo" src="' + file + '" type="video/mp4" id="player1" controls="controls" preload="none"></video></p>';
+				
+				}
+				
+				//now append it to the section
+				section.append(mediaStr);
+			}
+		});
+		
+		//a return to top button
+		section.append( $('<p><br><a class="btn btn-mini pull-right" href="#">Top</a></p>'));
+
+		//add the section to the document
+		$('#mainContent').append(section);
+					
+	});
+	
+	//finish initialising the piece now we have the content loaded
+	initMedia();
+	initSidebar();
+	
+	window.scroll(0,0);
+	
+	$('body').scrollspy('refresh');
+
+	//force facebook / twitter objects to initialise
+	twttr.widgets.load();
+	FB.XFBML.parse(); 
+
 }
