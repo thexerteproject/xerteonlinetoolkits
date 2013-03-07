bed6caf5dc0cd7d265c36d300a2e11ddeeec7022
John Smith <jjs@gcu.ac.uk>
2013-03-07 12:56:05 +0000

    SECURITY FIX: Patched upload to only allow whitelisted file extensions. This whitelist might not be best placed here though, and perhaps should be configurable in management.php ???
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@714 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- modules/xerte/engine/upload.php
+++ modules/xerte/engine/upload.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 
+ *
  * upload page, used by xerte to upload a file
  *
  * @author Patrick Lockley
@@ -11,26 +11,47 @@
 
 require_once("../../../config.php");
 
-file_put_contents('test.txt',var_export($_SESSION,TRUE));
-
-if(!isset($_SESSION['toolkits_logon_username'])) {
-  print "You are not logged in.";
- // exit();
+// Left in for now but restricted to only development mode
+global $development;
+if ($development == true) {
+	file_put_contents('test.txt', var_export($_SESSION,TRUE));
 }
 
-// SECURITY / TODO / XXX - someone can use this to upload an arbitrary file to a place of their choosing on the server 
-$pass = true;
+if(!isset($_SESSION['toolkits_logon_username'])) {
+  print "You are not logged in";
+  exit(); // ?? Why was this commented out in the v1.9 release ??
+}
+
+// Perhaps this whitelist should be moved to management.php or config.php??
+$media_whilelist = 'flv,mp4,mp3,jpg,jpeg,gif,png,swf'; // Pat's Wordpress list
+
+// Make sure extension is in the whitelist
+$pass = false;
+$allowed = explode(',', $media_whilelist);
+$extension = strtolower(pathinfo($_FILES['Filedata']['name'], PATHINFO_EXTENSION));
+foreach($allowed as $ext) {
+	if ($ext == $extension) {
+		$pass = true;
+		break;
+	}
+}
+
+// Not sure if we still need these but left in for now
 if (strpos($_FILES['Filedata']['name'], '../') !== false) $pass = false;
 if (strpos($_FILES['Filedata']['name'], '.exe') !== false) $pass = false;
 if (strpos($_FILES['Filedata']['name'], '...') !== false) $pass = false;
 
-if ($pass === false){
+// Block all files that haven't met the criteria
+if ($pass === false) {
   print "Invalid File Name";
   exit();
 }
 
 $new_file_name = $xerte_toolkits_site->root_file_path . $_GET['path'] . $_FILES['Filedata']['name'];
 
-if(move_uploaded_file($_FILES['Filedata']['tmp_name'], $new_file_name)){
-}else{
+if(move_uploaded_file($_FILES['Filedata']['tmp_name'], $new_file_name)) {
+
+}
+else {
+
 }
