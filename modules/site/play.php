9ba52162575b2191dc6d9904e300d491aacef185
Patrick Lockley <patrick.lockley@googlemail.com>
2013-04-15 23:51:12 +0000

    Fixed CSS and JS bug
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@774 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- modules/site/play.php
+++ modules/site/play.php
@@ -1,13 +1,16 @@
 <?php
-require(dirname(__FILE__) . "module_functions.php");
+require(dirname(__FILE__) . "/module_functions.php");
 
 //Function show_template
 //
 // Version 1.0 University of Nottingham
 // (pl)
 // Set up the preview window for a xerte piece
+
 require(dirname(__FILE__) .  '/../../website_code/php/xmlInspector.php');
+
 function show_template($row_play){
+
     global $xerte_toolkits_site;
 
     $string_for_flash = $xerte_toolkits_site-> users_file_area_short . $row_play['template_id'] . "-" . $row_play['username'] . "-" . $row_play['template_name'] . "/";
