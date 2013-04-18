077325cd37153869218a05145d00a7395651e330
Tom Reijnders <ajjreijnders@gmail.com>
2013-04-18 16:57:19 +0000

    - Missing files in the site template
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@799 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

---
+++ modules/site/engine/file_exists.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 
+ * file exists page, used by xerte to chec if a file is there before previewing it
+ *
+ * @author Patrick Lockley
+ * @version 1.0
+ * @copyright Copyright (c) 2008,2009 University of Nottingham
+ * @package
+ */
+
+require_once("../../../config.php");
+
+if(file_exists($xerte_toolkits_site->root_file_path . $_POST['file_name'])){
+    print("&return_value=true");
+}else{
+    print("&return_value=false");
+}
+
+
+?>
