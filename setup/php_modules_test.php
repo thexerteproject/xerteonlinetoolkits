a2f80e213e17dc8921a33f32079742c12138493f
Tom Reijnders <ajjreijnders@gmail.com>
2013-04-22 14:42:53 +0000

    - Simplified full install (a bit)
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@817 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- setup/php_modules_test.php
+++ setup/php_modules_test.php
@@ -1,5 +1,7 @@
 <?PHP
 echo file_get_contents("page_top");
+$ok = true;
+$warning=false;
 ?>
 <h2 style="margin-top:15px">
     Xerte Online Toolkits PHP modules checks
@@ -10,22 +12,98 @@ echo file_get_contents("page_top");
 <ol>
     <li><b>The PHP " File uploads" setting</b>
         <ul>
-            <li>Look in the Ini file for "file_uploads =" and set the value to be On</li>
-            <li>Look in the Ini file for "upload_tmp_dir =" and set the value to a path of your system outside of the area available from the web server (i.e if you are using XAMPP - do not put the temp directory in the HTDOCS folder)</li>
-            <li>Look in the Ini file for "upload_max_filesize =" and set the value to a that you want to be the maximum file size you can upload. The format for the setting is the number, then the letter 'M'.</li>
-            <li>Look in the Ini file for "post_max_size =" and set the value to a that you want to be the maximum file size you can upload (PHP advise you set this value to be slightly greater than the upload_max_filesize. The format for the setting is the number, then the letter 'M'.</li>
-            <li>Look in the Ini file for "memory_limit =" and set the value to a that you want to be the maximum file size you can upload (PHP advise you set this value to be slightly greater than the upload_max_filesize. The format for the setting is the number, then the letter 'M'.</li>
+            <li>Look in the Ini file for "file_uploads =" and set the value to be On :<?PHP if (ini_get("file_uploads") == 1){ echo "<div class=\"ok\">OK</div>";} else {echo "div class=\"error\">Off</div>"; $ok=false;} ?></li>
+            <li>Look in the Ini file for "upload_tmp_dir =" and set the value to a path of your system outside of the area available from the web server (i.e if you are using XAMPP - do not put the temp directory in the HTDOCS folder): <div class="info"><?php echo ini_get("upload_tmp_dir"); ?></div>
+                <?php if (ini_get("upload_tmp_dir") == "") {echo "div class=\"error\">Must be defined!</div>"; $ok=false;}?></li>
+            <li>Look in the Ini file for "upload_max_filesize =" and set the value to a that you want to be the maximum file size you can upload. The format for the setting is the number, then the letter 'M': <div class="info"><?php echo ini_get("upload_max_filesize");?></div></li>
+            <li>Look in the Ini file for "post_max_size =" and set the value to a that you want to be the maximum file size you can upload (PHP advise you set this value to be slightly greater than the upload_max_filesize. The format for the setting is the number, then the letter 'M': <div class="info"><?php echo ini_get("post_max_size");?></div></li>
+            <li>Look in the Ini file for "memory_limit =" and set the value to a that you want to be the maximum file size you can upload (PHP advise you set this value to be slightly greater than the upload_max_filesize. The format for the setting is the number, then the letter 'M': <div class="info"><?php echo ini_get("memory_limit");?></div></li>
         </ul>
     </li>
-    <li><b>The PHP "MySQL" Settings</b> - Please see <a href="http://uk3.php.net/manual/en/mysql.installation.php">PHP's own guide</a> for more details. Xampp installs should come with MySQL installed. Different versions of PHP however may or may not have MySQL installed by default. If on the PHP Info page you can find a section headed  "MySQL", then you should find it is installed.</li>
-    <li><b>The PHP "Sessions" Settings</b> - Please see <a href="http://uk2.php.net/manual/en/session.installation.php">PHP's own guide</a> for more details. Sessions should be turned on by default in a PHP install. Again, XAMPP users should find this is installed by default.<b>Some of the session file settings in index, integration and session.php have been commented out - you may wish to look at which settings work best for you </b>.</li>
-    <li><b>The PHP "LDAP" Settings</b> - Please see <a href="http://uk2.php.net/manual/en/ldap.setup.php">PHP's own guide</a> for more details. If you don't want to use LDAP you could write your own authentication code, or refer to the readme.txt in the main install folder on how to run an LDAP free version of the code.</li>
-    <li><b>The PHP "Mail" Settings</b> - Please see <a href="http://uk2.php.net/manual/en/mail.setup.php">PHP's own guide</a> for more details. As the page lists, you may need to set the following variables - <b>SMTP, smtp_port, sendmail_from </b> and <b>sendmail_path</b>. Should you wish to, you can run the code without mail, but some modifications to the feedback and version control pages would be required.</li>	
-    <li><b>The PHP "Zlib" Settings</b> - Please see <a href="http://uk2.php.net/manual/en/zlib.setup.php">PHP's own guide</a> for more details. If you wish to export projects or make SCORM packages, you will need this library to be installed.</li>	
+
+    <li>
+        <b>The PHP "Sessions" Settings</b> -
+        <?php if(function_exists("session_start"))
+        {
+            echo "<div class=\"ok\">OK</div>";
+        }
+        else
+        {
+            $ok = false;
+            echo "<div class=\"error\"><p>Please see <a href=\"http://uk2.php.net/manual/en/session.installation.php\">PHP's own guide</a> for more details. Sessions should be turned on by default in a PHP install. Again, XAMPP users should find this is installed by default.<b>Some of the session file settings in index, integration and session.php have been commented out - you may wish to look at which settings work best for you </b>.</p></div>";
+        }
+        ?>
+    </li>
+    <li>
+        <b>The PHP "LDAP" Settings</b> -
+        <?php if(function_exists("ldap_connect"))
+        {
+            echo "<div class=\"ok\">OK</div>";
+        }
+        else
+        {
+            $warning = true;
+            echo "<div class=\"info\"><p>Please see <a href=\"http://uk2.php.net/manual/en/ldap.setup.php\">PHP's own guide</a> for more details. If you don't want to use LDAP you can continue with the installation. Make sure to choose a different authentication method.</p></div>";
+        }
+        ?>
+    </li>
+    <li>
+        <b>The PHP "Mail" Settings</b> -
+        <?php if(ini_get("SMTP") != "")
+        {
+            echo "<div class=\"ok\">Probably OK</div>";
+        }
+        else
+        {
+            $warning = true;
+        }
+        echo "<div class=\"info\">";
+        echo "<p>Please see <a href=\"http://uk2.php.net/manual/en/mail.setup.php\">PHP's own guide</a> for more details. As the page lists, you may need to set the following variables - </p>";
+        echo "<ul>";
+        echo "<li><b>SMTP</b> - " . ini_get("SMTP") . "</li>";
+        echo "<li><b>smtp_port</b> - " . ini_get("smtp_port") . "</li>";
+        echo "<li><b>sendmail_from</b> - " . ini_get("sendmail_from") . "</li>";
+        echo "<li><b>sendmail_path</b> = " . ini_get("sendmail_path") . "</li>";
+        echo "</ul>";
+        echo "<p>Should you wish to, you can run the code without mail, but some modifications to the feedback and version control pages would be required.</p></div>";
+        ?>
+    </li>
+    <li>
+        <b>The PHP "Zlib" Settings</b> -
+        <?php if(function_exists("gzcompress"))
+        {
+            echo "<div class=\"ok\">OK</div>";
+        }
+        else
+        {
+            $ok = false;
+            echo "<div class=\"error\"><p>Please see <a href=\"http://uk2.php.net/manual/en/zlib.setup.php\">PHP's own guide</a> for more details. If you wish to export projects or make SCORM packages, you will need this library to be installed.</p></div>";
+        }
+        ?>
+    </li>
 </ol>	
 
 </p>
-<iframe width="900" height="300" src="iframe_php.php"></iframe><br>
-If no errors are listed above, please proceed to the <a href="mysql_test.php">next test</a>.
+<p>
+<?php
+if ($warning)
+{
+    echo "Warnings were issued but you can continue the installation.";
+}
+?>
+</p>
+<p>
+<?php
+if ($ok)
+{
+    echo "<form action=\"page1.php\"><input type=\"submit\" value=\"Next\" ></form>";
+}
+else{
+    echo "<form action=\"php_modules_test.php\"><input type=\"submit\" value=\"Try again\"></form>";
+}
+?>
+</p>
+
+
 </body>
 </html>
