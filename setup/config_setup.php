f69c67812677cea410ac1f04b8050eb23935cb2c
Tom Reijnders <ajjreijnders@gmail.com>
2013-04-22 14:42:53 +0000

    - Simplified full install (a bit)
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@817 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- setup/config_setup.php
+++ setup/config_setup.php
@@ -12,6 +12,7 @@ echo file_get_contents("page_top");
 <ol>
     <li>A PHP Server running version 5.2 or above (Xerte Online Toolkits was developed on PHP 5.2+, it may work on older versions. It may not.) - 
 <?php
+$ok = true;
 $version = phpversion();
 if (version_compare($version, "5.2.0", "<")) {
     echo "<div class='error'>Your version of PHP is older than 5.2.0. ($version)</div>";
@@ -24,7 +25,8 @@ if (version_compare($version, "5.2.0", "<")) {
     <li>A MYSQL Install (Xerte Online Toolkits was developed on ver 14.12 Distrib 5.05.51a for Win32. We haven't tested this with other versions, or on other database systems)
         <?php
         if (!function_exists('mysql_connect')) {
-            echo "<div class='error'>Your  PHP does not seem to have MySQL support </div>";
+            echo "<div class='error'><p>Your  PHP does not seem to have MySQL support</p><p>Please see <a href=\"http://uk3.php.net/manual/en/mysql.installation.php\">PHP's own guide</a> for more details. Xampp installs should come with MySQL installed. Different versions of PHP however may or may not have MySQL installed by default. If on the PHP Info page you can find a section headed  \"MySQL\", then you should find it is installed.</p> </div>";
+            $ok = false;
         } else {
             echo "<div class='ok'>MySQL support present</div>";
         }
@@ -39,79 +41,19 @@ if (version_compare($version, "5.2.0", "<")) {
 </p>
 <p>At present, with the files positioned as they are, you will install the system into <?PHP echo substr(getcwd(), 0, strlen(getcwd()) - 5); ?> and you will have a system with the web address of http://<?PHP echo $_SERVER['HTTP_HOST'] . substr($_SERVER['PHP_SELF'], 0, strlen($_SERVER['PHP_SELF']) - 22); ?>. <br><br><b>Do not proceed with the installer unless you have these as the installer will fail</b>.</p>
 <p>
-    The are some settings which must be in place before starting the installer. You should set the file permissions on the following folders as the page specifies. 
-<ol>
+The next pages will help you verify and solve system issues. You will not be able to continue until all requirements are fulfilled.
+</p>
+
+<p>
 <?php
-$root = dirname(dirname(__FILE__));
+    if ($ok)
+    {
+        echo "<form action=\"file_system_test.php\"><input type=\"submit\" value=\"Next\" ></form>";
+    }
+    else{
+        echo "<form action=\"config_setup.php\"><input type=\"submit\" value=\"Try again\"></form>";
+    }
 ?>
-    <li>The root folder for this install (<?php echo $root; ?>) must be set to chmod 0777 / Full write access. 
-<?php if (is_writeable($root)) {
-    echo "<div class='ok'>OK</div>";
-} else {
-    echo "<div class='error'>Please fix</div>";
-} ?>
-    </li>
-    <li>The setup folder for this install (<?PHP echo dirname(__FILE__); ?>) must be set to chmod 0777 / Full write access.
-        <?php if (is_writeable(dirname(__FILE__))) {
-            echo "<div class='ok'>OK</div>";
-        } else {
-            echo "<div class='error'>Please fix</div>";
-        } ?>
-
-    </li>
-    <li>The user files folder for this install (<?PHP echo substr(getcwd(), 0, strlen(getcwd()) - 5) . "USER-FILES"; ?>) must be set to chmod 0777 / Full write access.
-<?php if (is_writeable($root . DIRECTORY_SEPARATOR . 'USER-FILES')) {
-    echo "<div class='ok'>OK</div>";
-} else {
-    echo "<div class='error'>Please fix</div>";
-} ?>
-
-    </li>
-    <li>The error log folder for this install (<?PHP echo substr(getcwd(), 0, strlen(getcwd()) - 5) . "error_logs"; ?>) must be set to chmod 0777 / Full write access.</li>
-<?php if (is_writeable($root . DIRECTORY_SEPARATOR . 'error_logs')) {
-    echo "<div class='ok'>OK</div>";
-} else {
-    echo "<div class='error'>Please fix</div>";
-} ?>
-    <li>The import folder for this install (<?PHP echo substr(getcwd(), 0, strlen(getcwd()) - 5) . "import"; ?>) must be set to chmod 0777 / Full write access.</li>
-<?php if (is_writeable($root . DIRECTORY_SEPARATOR . 'import')) {
-    echo "<div class='ok'>OK</div>";
-} else {
-    echo "<div class='error'>Please fix</div>";
-} ?>
-
-</ol>
-Once the installer has finished, you can set the folder permissions to your own preferences - except for USER-FILES, error_logs and import, to which the web server will still need write / read and delete access. People testing locally do not need to worry about these settings.
-</p>
-
-<h2>Further Installation Guidance</h2>
-<p>
-    Use your <a href="phpinfo.php" target="_blank">PHP info page</a> to find the 'Loaded Configuration File' (look on the first part of the php info page for the text 'Loaded Configuration File' - the use this path to find the file. Make a copy of it before you start. You can edit this file in notepad, or any text editor. People following the XAMPP path should find that they do not need to make any of these changes to make their system work.
-<ol>
-    <li><b>The PHP "Short tags" setting</b> - Look in the Ini file for "short_open_tag =" and set the value to be On. At present your PHP install has this setting turned  (<?PHP if (ini_get("short_open_tag") == 1) {
-    echo "<div class='ok'>on</div>";
-} else {
-    echo "<div class='error'>off</div>";
-} ?>).</li>
-    <li><b>The PHP " File uploads" setting</b>
-        <ul>
-            <li>Look in the Ini file for "file_uploads =" and set the value to be On</li>
-            <li>Look in the Ini file for "upload_tmp_dir =" and set the value to a path of your system outside of the area available from the web server (i.e if you are using XAMPP - do not put the temp directory in the HTDOCS folder)</li>
-            <li>Look in the Ini file for "upload_max_filesize =" and set the value to a that you want to be the maximum file size you can upload. The format for the setting is the number, then the letter 'M'.</li>
-            <li>Look in the Ini file for "post_max_size =" and set the value to a that you want to be the maximum file size you can upload (PHP advise you set this value to be slightly greater than the upload_max_filesize. The format for the setting is the number, then the letter 'M'.</li>
-            <li>Look in the Ini file for "memory_limit =" and set the value to a that you want to be the maximum file size you can upload (PHP advise you set this value to be slightly greater than the upload_max_filesize. The format for the setting is the number, then the letter 'M'.</li>
-        </ul>
-    </li>
-    <li><b>The PHP "MySQL" Settings</b> - Please see <a href="http://php.net/manual/en/mysql.installation.php">PHP's own guide</a> for more details. Xampp installs should come with MySQL installed. Different versions of PHP however may or may not have MySQL installed by default. If on the PHP Info page you can find a section headed  "MySQL", then you should find it is installed.</li>
-    <li><b>The PHP "Sessions" Settings</b> - Please see <a href="http://php.net/manual/en/session.installation.php">PHP's own guide</a> for more details. Sessions should be turned on by default in a PHP install. Again, XAMPP users should find this is installed by default.<b>Some of the session file settings in index, integration and session.php have been commented out - you may wish to look at which settings work best for you </b>.</li>
-    <li><b>The PHP "LDAP" Settings</b> - Please see <a href="http://php.net/manual/en/ldap.setup.php">PHP's own guide</a> for more details. If you don't want to use LDAP you could write your own authentication code, or refer to the readme.txt in the main install folder on how to run an LDAP free version of the code.</li>
-    <li><b>The PHP "Mail" Settings</b> - Please see <a href="http://php.net/manual/en/mail.setup.php">PHP's own guide</a> for more details. As the page lists, you may need to set the following variables - <b>SMTP, smtp_port, sendmail_from </b> and <b>sendmail_path</b>. Should you wish to, you can run the code without mail, but some modifications to the feedback and version control pages would be required.</li>	
-    <li><b>The PHP "Zlib" Settings</b> - Please see <a href="http://php.net/manual/en/zlib.setup.php">PHP's own guide</a> for more details. If you wish to export projects or make SCORM packages, you will need this library to be installed.</li>	
-</ol>	
-
-</p>
-<p>
-    <a href="file_system_test.php" style="border:0; text-decoration:none"><img src="next.gif" /></a>
 </p>
 </body>
 </html>
--- setup/file_system_test.php
+++ setup/file_system_test.php
@@ -7,14 +7,72 @@ echo file_get_contents("page_top");
 <p>
     The are some settings which must be in place before starting the installer. You should set the file permissions on the following folders as the page specifies. 
 <ol>
-    <li>The root folder for this install (<?PHP echo substr(getcwd(), 0, strlen(getcwd()) - 5); ?>) must be set to chmod 0777 / Full write access.</li>
-    <li>The setup	folder for this install (<?PHP echo substr(getcwd(), 0, strlen(getcwd())); ?>) must be set to chmod 0777 / Full write access.</li>
-    <li>The user files folder for this install (<?PHP echo substr(getcwd(), 0, strlen(getcwd()) - 5) . "USER-FILES"; ?>) must be set to chmod 0777 / Full write access.</li>
-    <li>The error log folder for this install (<?PHP echo substr(getcwd(), 0, strlen(getcwd()) - 5) . "error_logs"; ?>) must be set to chmod 0777 / Full write access.</li>
-    <li>The import folder for this install (<?PHP echo substr(getcwd(), 0, strlen(getcwd()) - 5) . "import"; ?>) must be set to chmod 0777 / Full write access.</li>
+    <?php
+    $root = dirname(dirname(__FILE__));
+    $ok = true;
+    ?>
+
+    <li>
+        The root folder for this install (<?PHP echo $root; ?>) must be writable during setup.
+        <?php if (is_writeable($root)) {
+            echo "<div class='ok'>OK</div>";
+        } else {
+            echo "<div class='error'><p>Please fix by changing the permission to 0777 or changing the ownership to the user account that runs the webserver.</p></div>";
+            $ok = false;
+        } ?>
+    </li>
+    <li>
+        The setup folder for this install (<?PHP echo dirname(__FILE__); ?>) must be writable during setup.
+        <?php if (is_writeable(dirname(__FILE__))) {
+            echo "<div class='ok'>OK</div>";
+        } else {
+            echo "<div class='error'><p>Please fix by changing the permission to 0777 or changing the ownership to the user account that runs the webserver.</p></div>";
+            $ok = false;
+        } ?>
+    </li>
+    <li>
+        The user files folder for this install (<?PHP echo $root . "/USER-FILES"; ?>) must be writable.
+        <?php if (is_writeable($root . "/USER-FILES")) {
+            echo "<div class='ok'>OK</div>";
+        } else {
+            echo "<div class='error'><p>Please fix by changing the permission to 0777 or changing the ownership to the user account that runs the webserver.</p></div>";
+            $ok = false;
+        } ?>
+    </li>
+    <li>
+        The error log folder for this install (<?PHP echo $root . "/error_logs"; ?>) must be writable.
+        <?php if (is_writeable($root . "/error_logs")) {
+            echo "<div class='ok'>OK</div>";
+        } else {
+            echo "<div class='error'><p>Please fix by changing the permission to 0777 or changing the ownership to the user account that runs the webserver.</p></div>";
+            $ok = false;
+        } ?>
+    </li>
+    <li>
+        The import folder for this install (<?PHP echo $root . "/import"; ?>) must be writable.
+        <?php if (is_writeable($root . "/import")) {
+            echo "<div class='ok'>OK</div>";
+        } else {
+            echo "<div class='error'><p>Please fix by changing the permission to 0777 or changing the ownership to the user account that runs the webserver.</p></div>";
+            $ok = false;
+        } ?>
+    </li>
 </ol>
 </p>
-<iframe width="900" height="300" src="iframe_file.php"></iframe><br>
-If no errors are listed above, please take the <a href="php_modules_test.php">next test</a>. If problems have occurred then please refer to the install guide or the resources available on the <a href="http://www.nottingham.ac.uk/xerte">Xerte website</a>.
+<?php
+    if ($ok)
+    {
+        echo "<form action=\"php_modules_test.php\"><input type=\"submit\" value=\"Next\" ></form>";
+    }
+    else{
+        echo "<form action=\"file_system_test.php\"><input type=\"submit\" value=\"Try again\"></form>";
+    }
+?>
+<p>
+    Once the installer has finished, you can set the folder permissions to your own preferences - except for USER-FILES, error_logs and import, to which the web server will still need write / read and delete access. People testing locally do not need to worry about these settings.
+</p>
+<p>
+If problems have occurred then please refer to the install guide or the resources available on the <a href="http://www.xerte.org.uk">Xerte Community Website</a>.
+</p>
 </body>
 </html>
