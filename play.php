7f4d8b28a43df0dd2e539a435101f8f277720a6e
John Smith <jjs@gcu.ac.uk>
2013-06-30 11:05:31 +0000

    Fix for NULL number_of_uses in v2.0 branch
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/branches/2.0@1014 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- play.php
+++ play.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * 
+ *
  * Play page, displays the template to the end user
  *
  * @author Patrick Lockley
@@ -25,11 +25,11 @@ require_once $xerte_toolkits_site->php_library_path . "template_library.php";
 //ini_set(display_errors,"ON");
 
 /**
- * 
+ *
  * Function check_host
  * This function checks http security settings
  * @param string $hostname = should be the $SERVER referrer
- * @param string $setting = what the security setting in play_security_details is 
+ * @param string $setting = what the security setting in play_security_details is
  * @return bool True or False if two params match
  * @version 1.0
  * @author Patrick Lockley
@@ -45,11 +45,11 @@ function check_host($hostname, $setting)
 }
 
 /**
- * 
+ *
  * Function check_ip
  * This function checks IP security settings
  * @param string $hostname = should be the $_SERVER REMOTE_ADDR
- * @param string $setting = what the security setting in play_security_details is 
+ * @param string $setting = what the security setting in play_security_details is
  * @return bool True or False if two params match
  * @version 1.0
  * @author Patrick Lockley
@@ -110,10 +110,10 @@ function check_ip($ip_address, $security_settings)
 }
 
 /**
- * 
+ *
  * Function check_security_type
  * This function checks database settings to see if non standard play security options have been met
- * @param string $security_setting = the value taken from security_setting in play_security_details 
+ * @param string $security_setting = the value taken from security_setting in play_security_details
  * @return bool True or False if two params match
  * @version 1.0
  * @author Patrick Lockley
@@ -193,6 +193,13 @@ require_once $xerte_toolkits_site->php_library_path . "screen_size_library.php";
 */
 require_once $xerte_toolkits_site->root_file_path . "modules/" . $row_play['template_framework'] . "/play.php";
 
+
+/*
+ * Fix for NULL number_of_uses
+ */
+db_query("UPDATE {$xerte_toolkits_site->database_table_prefix}templatedetails SET number_of_uses = 0 WHERE ISNULL(number_of_uses)");
+
+
 /*
  * Start to check the access_to_whom settings from templatedetails for this template
  */
@@ -204,7 +211,7 @@ if ($row_play['access_to_whom'] == "Private") {
      */
 
     dont_show_template();
-	
+
 } else if ($row_play['access_to_whom'] == "Public") {
 
     /*
@@ -214,7 +221,7 @@ if ($row_play['access_to_whom'] == "Private") {
     db_query("UPDATE {$xerte_toolkits_site->database_table_prefix}templatedetails SET number_of_uses=number_of_uses+1 WHERE template_id=?", array($safe_template_id));
 
     show_template($row_play);
-	
+
 } else if ($row_play['access_to_whom'] == "Password") {
 
     /*
@@ -285,7 +292,7 @@ if($lti->valid) {
             require $xerte_toolkits_site->root_file_path . "modules/" . $row_play['template_framework'] . "/play.php";
 
             show_template($row_play);
-			
+
         } else {
 
             /*
@@ -323,7 +330,7 @@ if($lti->valid) {
             db_query("UPDATE {$xerte_toolkits_site->database_table_prefix}templatedetails SET number_of_uses=number_of_uses+1 WHERE template_id=?", array($safe_template_id));
 
             show_template($row_play);
-			
+
         } else {
             dont_show_template('Doesnt Match Referer:' . $_SERVER['HTTP_REFERER']);
         }
@@ -353,7 +360,7 @@ if($lti->valid) {
                 $flag = true;
 
                 break;
-				
+
             } else {
 
                 $flag == false;
