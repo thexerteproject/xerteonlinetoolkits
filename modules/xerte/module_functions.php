c4ccf8d791932ca3776c38bfdb3f21f3285e1232
Tom Reijnders <ajjreijnders@gmail.com>
2013-08-02 11:38:55 +0000

    Cosmetic fix to use logo's set in site_details
    git-svn-id: https://xerteonlinetoolkits.googlecode.com/svn/trunk@1073 912cdd6b-5c7d-d5a7-a2ba-d0f0cdb91641

--- modules/xerte/module_functions.php
+++ modules/xerte/module_functions.php
@@ -50,8 +50,16 @@ function display_publish_engine(){
 
 function dont_show_template($optional=''){
 
-    _load_language_file("/modules/xerte/module_functions.inc");
+    global $xerte_toolkits_site;
 
+    _load_language_file("/modules/xerte/module_functions.inc");
+    $edit_site_logo = $xerte_toolkits_site->site_logo;
+    $pos = strrpos($edit_site_logo, '/') + 1;
+    $edit_site_logo = substr($edit_site_logo,0,$pos) . "edit_" . substr($edit_site_logo,$pos);
+
+    $edit_organisational_logo = $xerte_toolkits_site->organisational_logo;
+    $pos = strrpos($edit_organisational_logo, '/') + 1;
+    $edit_organisational_logo = substr($edit_organisational_logo,0,$pos) . "edit_" . substr($edit_organisational_logo,$pos);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html xmlns="http://www.w3.org/1999/xhtml">
@@ -66,9 +74,9 @@ function dont_show_template($optional=''){
 
     <div style="margin:0 auto; width:800px">
     <div class="edit_topbar" style="width:800px">
-        <img src="website_code/images/edit_xerteLogo.jpg" style="margin-left:10px; float:left" />
-        <img src="website_code/images/edit_UofNLogo.jpg" style="margin-right:10px; float:right" />
-    </div>	
+        <img src="<?php echo $edit_site_logo;?>" style="margin-left:10px; float:left" />
+        <img src="<?php echo $edit_organisational_logo;?>" style="margin-right:10px; float:right" />
+    </div>
     <div style="margin:0 auto">
 <?PHP
 
