53947bc82765608c7f1c49481818e4d99326e44d
Tom Reijnders <ajjreijnders@gmail.com>
2013-09-05 14:02:16 +0200

    Fix typo

--- build/rebuildNottingham.bat
+++ build/rebuildNottingham.bat
@@ -1,3 +1,4 @@
 @echo off
 php\php.exe ..\website_code\php\rebuildtemplate.php ..\src\Nottingham\wizards\en-GB > convert.log
-cp ..\src\Nottingham\wizards\en-GB\template.xwd ..\modules\xerte\parent_templates\Nottingham\wizards\en-GB\data.xwd
+copy ..\src\Nottingham\wizards\en-GB\template.xwd ..\modules\xerte\parent_templates\Nottingham\wizards\en-GB\data.xwd
+
