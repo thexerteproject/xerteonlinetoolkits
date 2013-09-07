988984b718990df7d68537deccb81d315cacb175
JohnSmith-LT <J.J.Smith@gcu.ac.uk>
2013-09-07 09:20:03 +0100

    Wrap MathJax Queue calls to catch error when LO running offline - MathJax wont work offline...
    Signed-off-by: JohnSmith-LT <J.J.Smith@gcu.ac.uk>

--- modules/xerte/parent_templates/Nottingham/common_html5/js/xenith.js
+++ modules/xerte/parent_templates/Nottingham/common_html5/js/xenith.js
@@ -755,7 +755,8 @@ function x_changePage(x_gotoPage) {
 		$("#x_page" + x_currentPage).load(x_templateLocation + "models_html5/" + x_pageInfo[x_currentPage].type + ".html", x_loadPage);
 	}
 	
-	MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+	// Queue reparsing of MathJax - fails if no network connection
+	try { MathJax.Hub.Queue(["Typeset",MathJax.Hub]); } catch (e){}
 	
 	x_updateHash();
 }
@@ -790,8 +791,8 @@ function x_loadPage(response, status, xhr) {
 		x_pageLoaded();
 	}
 	
-    	// Queue reparsing of MathJax
-    	MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+    	// Queue reparsing of MathJax - fails if no network connection
+    	try { MathJax.Hub.Queue(["Typeset",MathJax.Hub]); } catch (e){}
 	
 	x_setUpPage();
 }
