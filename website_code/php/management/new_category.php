79b91f057b94c6f4437b84e70948d6418834351a
Julian Tenney <julian.tenney@nottingham.ac.uk>
2013-09-20 14:13:43 +0100

    PAt's fixes

--- website_code/php/management/new_category.php
+++ website_code/php/management/new_category.php
@@ -7,8 +7,8 @@ require("management_library.php");
 
 if(is_user_admin()){
 
-    $query = "INSERT INTO {$xerte_toolkits_site->database_table_prefix}syndicationcategories (category_name) values  (?)";
-    $res = mysql_query($query, array($_POST['newcategory']));
+    $query = "INSERT INTO {$xerte_toolkits_site->database_table_prefix}syndicationcategories (category_name) values (?)";
+    $res = db_query($query, array($_POST['newcategory']));
 
     if($res) {
 
