297e9dd9394956a6998899271b4df6e3c7110728
Mark Berthelemy <mberthelemy@wyversolutions.co.uk>
2013-12-03 15:27:27 +0000

    Removed setup folder from languages/en-GB

--- languages/en-GB/setup/iframe_file.php
+++
@@ -1,7 +0,0 @@
-<html>
-	<body>
-		<form action="file_system_iframe.php">
-			<input type="submit" value="Run file system test" />
-		</form>
-	</body>
-</html>
--- languages/en-GB/setup/iframe_php.php
+++
@@ -1,7 +0,0 @@
-<html>
-	<body>
-		<form action="php_modules_iframe.php">
-			<input type="submit" value="Run PHP module test" />
-		</form>
-	</body>
-</html>
