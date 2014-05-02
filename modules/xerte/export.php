696d0323c1e47fa52daf8832103d0fd77611431e
David Goodwin <david@palepurple.co.uk>
2014-05-02 15:22:08 +0100

    reindent, use Xerte_Zip_Factory::factory() to solve memory issue(s) with zip file creation; use tempnam() for tmp file

--- modules/xerte/export.php
+++ modules/xerte/export.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Export a LO - e.g. from properties.
+ * Example call : /website_code/php/scorm/export.php?scorm=false&template_id=10&html5=false&flash=true
+ */
 global $dir_path, $delete_file_array, $zipfile, $folder_id_array, $file_array, $folder_array, $delete_folder_array, $parent_template_path;
 
 $folder_id_array = array();
@@ -28,63 +32,59 @@ $scorm2004_path = $xerte_toolkits_site->basic_template_path . $row['template_fra
 $scorm2004_language_relpath = $xerte_toolkits_site->module_path . $row['template_framework'] . "/scorm2004.3rd/";
 $js_path = $xerte_toolkits_site->basic_template_path . $row['template_framework'] . "/js/";
 
-if(isset($_REQUEST['html5']))
-{
+if (isset($_REQUEST['html5'])) {
     $export_html5 = ($_REQUEST['html5'] == 'true' ? true : false);
 }
-if(isset($_REQUEST['flash']))
-{
+if (isset($_REQUEST['flash'])) {
     $export_flash = ($_REQUEST['flash'] == 'true' ? true : false);
 }
-if (!$export_html5 && !$export_flash)
-{
-    if (isset($row['extra_flags']))
-    {
-        if (strpos($row['extra_flags'], 'flash') !== false)
-        {
+if (!$export_html5 && !$export_flash) {
+    if (isset($row['extra_flags'])) {
+        if (strpos($row['extra_flags'], 'flash') !== false) {
             $export_flash = true;
-        }
-        else
-        {
+        } else {
             $export_html5 = true;
         }
-    }
-    else
-    {
-        $export_html5=true;
+    } else {
+        $export_html5 = true;
     }
 }
 
 /*
  * Make the zip
  */
-$zipfile_tmpname =  tempnam(sys_get_temp_dir());
-$zipfile = new zip_file($zipfile_tmpname);
-$zipfile->set_options(
-        array('basedir' => $dir_path, 
-            'prepand' => "", 'inmemory' => 0, 
-            'recurse' => 1, 'storepaths' => 1));
+$zipfile_tmpname = tempnam(sys_get_temp_dir(), 'xerteExport');
+_debug("Temporary zip file is : $zipfile_tmpname");
+
+
+$options = array(
+    'basedir' => $dir_path, 
+    'prepand' => "", 
+    'inmemory' => 0, 
+    'overwrite' => 1,
+    'recurse' => 1, 
+    'storepaths' => 1);
+
+
+$zipfile = Xerte_Zip_Factory::factory($zipfile_tmpname, $options);
+
 
 /*
  * Copy the core files over from the parent folder
  */
-copy($dir_path . "data.xml",$dir_path . "template.xml");
+copy($dir_path . "data.xml", $dir_path . "template.xml");
 $xml = new XerteXMLInspector();
 $xml->loadTemplateXML($dir_path . 'template.xml');
 
-if ($fullArchive){
+if ($fullArchive) {
     _debug("Full archive");
     export_folder_loop($parent_template_path);
-}
-else /* Only copy used models and the common folder */
-{
+} else /* Only copy used models and the common folder */ {
     _debug("Deployment archive");
-    if ($export_flash)
-    {
+    if ($export_flash) {
         _debug("  use flash");
         $models = $xml->getUsedModels();
-        foreach($models as $model)
-        {
+        foreach ($models as $model) {
             _debug("copy model " . $parent_template_path . "models/" . $model . ".rlm");
             array_push($file_array, array($parent_template_path . "models/" . $model . ".rlm", ""));
         }
@@ -94,12 +94,10 @@ else /* Only copy used models and the common folder */
 
         export_folder_loop($parent_template_path . "common/");
     }
-    if ($export_html5)
-    {
+    if ($export_html5) {
         _debug("  use html5");
         $models = $xml->getUsedModels();
-        foreach($models as $model)
-        {
+        foreach ($models as $model) {
             _debug("copy model " . $parent_template_path . "models_html5/" . $model . ".html");
             array_push($file_array, array($parent_template_path . "models_html5/" . $model . ".html", ""));
         }
@@ -111,16 +109,15 @@ else /* Only copy used models and the common folder */
         array_push($file_array, array($parent_template_path . "models_html5/colourChanger.html", ""));
 
         export_folder_loop($parent_template_path . "common_html5/");
-
     }
     array_push($file_array, array($parent_template_path . $row['template_name'] . ".rlt", ""));
 }
-if(isset($_GET['local'])){
-    if($_GET['local']=="true"){
+if (isset($_GET['local'])) {
+    if ($_GET['local'] == "true") {
         $string = file_get_contents($dir_path . "/template.xml");
         $string = str_replace($xerte_toolkits_site->site_url . $xerte_toolkits_site->users_file_area_short . $row['template_id'] . "-" . $row['username'] . "-" . $row['template_name'] . "/", "", $string);
         $fh = fopen($dir_path . "/template.xml", 'w+');
-        fwrite($fh,$string);
+        fwrite($fh, $string);
         fclose($fh);
     }
 }
@@ -133,8 +130,7 @@ copy_parent_files();
 export_folder_loop($xerte_toolkits_site->root_file_path . 'languages/', false, '.xml');
 copy_extra_files();
 
-if ($export_flash)
-{
+if ($export_flash) {
     /*
      * Javascript js folder
      */
@@ -147,38 +143,32 @@ if ($export_flash)
      *  From root
      */
     copy($xerte_toolkits_site->root_file_path . "XMLEngine.swf", $dir_path . "XMLEngine.swf");
-    array_push($delete_file_array,  $dir_path . "XMLEngine.swf");
+    array_push($delete_file_array, $dir_path . "XMLEngine.swf");
     copy($xerte_toolkits_site->root_file_path . "MainPreloader.swf", $dir_path . "MainPreloader.swf");
-    array_push($delete_file_array,  $dir_path . "MainPreloader.swf");
+    array_push($delete_file_array, $dir_path . "MainPreloader.swf");
     copy($xerte_toolkits_site->root_file_path . "rloObject.js", $dir_path . "rloObject.js");
-    array_push($delete_file_array,  $dir_path . "rloObject.js");
+    array_push($delete_file_array, $dir_path . "rloObject.js");
     copy($xerte_toolkits_site->root_file_path . "resources.swf", $dir_path . "resources.swf");
-    array_push($delete_file_array,  $dir_path . "resources.swf");
+    array_push($delete_file_array, $dir_path . "resources.swf");
 }
 /*
  * If scorm copy the scorm files as well
  */
-$scorm=mysql_real_escape_string($_GET['scorm']);
-if($scorm=="true")
-{
+$scorm = mysql_real_escape_string($_GET['scorm']);
+if ($scorm == "true") {
     export_folder_loop($scorm_path, false, null, "/");
     copy_extra_files();
-    if ($xml->getLanguage() != 'en-GB')
-    {
+    if ($xml->getLanguage() != 'en-GB') {
         export_folder_loop($xerte_toolkits_site->root_file_path . 'languages/' . $xml->getLanguage() . '/' . $scorm_language_relpath, false, null, "/languages/js/" . $xml->getLanguage() . "/");
         copy_extra_files();
     }
     export_folder_loop($xerte_toolkits_site->root_file_path . 'languages/en-GB/' . $scorm_language_relpath, false, null, "/languages/js/en-GB/");
     copy_extra_files();
-}
-else if ($scorm=="2004")
-{
+} else if ($scorm == "2004") {
     export_folder_loop($scorm2004_path, false, null, "/");
     copy_extra_files();
-    if ($xml->getLanguage() != 'en-GB')
-    {
-        if (is_dir($xerte_toolkits_site->root_file_path . 'languages/' . $xml->getLanguage() . '/' . $scorm2004_language_relpath))
-        {
+    if ($xml->getLanguage() != 'en-GB') {
+        if (is_dir($xerte_toolkits_site->root_file_path . 'languages/' . $xml->getLanguage() . '/' . $scorm2004_language_relpath)) {
             export_folder_loop($xerte_toolkits_site->root_file_path . 'languages/' . $xml->getLanguage() . '/' . $scorm2004_language_relpath, false, null, "/languages/js/" . $xml->getLanguage() . "/");
             copy_extra_files();
         }
@@ -194,24 +184,21 @@ $rlo_file = $row['template_name'] . ".rlt";
 /*
  *  jmol
  */
-if ($xml->modelUsed("jmol"))
-{
+if ($xml->modelUsed("jmol")) {
     export_folder_loop($xerte_toolkits_site->root_file_path . "JMolViewer/");
     copy_extra_files();
 }
 /*
  * mapstraction
  */
-if ($xml->modelUsed("mapstraction"))
-{
+if ($xml->modelUsed("mapstraction")) {
     export_folder_loop($xerte_toolkits_site->root_file_path . "mapstraction/");
     copy_extra_files();
 }
 /*
  * mediaViewer
  */
-if ($xml->mediaIsUsed())
-{
+if ($xml->mediaIsUsed()) {
     export_folder_loop($xerte_toolkits_site->root_file_path . "mediaViewer/");
     copy_extra_files();
 }
@@ -226,55 +213,45 @@ $lo_name = $xml->getName();
 /*
  * Create scorm manifests or a basic HTML page
  */
-if($scorm=="true"){
+if ($scorm == "true") {
     $useflash = ($export_flash && !$export_html5);
-    if(isset($_GET['data'])){
-        if($_GET['data']==true){
+    if (isset($_GET['data'])) {
+        if ($_GET['data'] == true) {
 
             $prefix = $xerte_toolkits_site->database_table_prefix;
-            
+
             $query = "SELECT * FROM {$prefix}templatesyndication WHERE template_id = ? ";
-            $metadata = db_query_one($query, array($_GET['template_id'])); 
-            
+            $metadata = db_query_one($query, array($_GET['template_id']));
+
             $query = "SELECT * FROM {$prefix}templaterights "
-            . "{$prefix}logindetails  WHERE template_id = ? and login_id = user_id ";
-            
+                    . "{$prefix}logindetails  WHERE template_id = ? and login_id = user_id ";
+
             $users = db_query($query, array($_GET['template_id']));
-            
+
             $query_response_users = mysql_query($query);
             lmsmanifest_create_rich($row, $metadata, $users, $useflash, $lo_name);
         }
-    }
-    else
-    {
+    } else {
         lmsmanifest_create($row['zipname'], $useflash, $lo_name);
     }
-    if ($useflash)
-    {
-        scorm_html_page_create($row['template_name'],$row['template_framework'], $rlo_file, $lo_name, $xml->getLanguage());
-    }
-    else
-    {
+    if ($useflash) {
+        scorm_html_page_create($row['template_name'], $row['template_framework'], $rlo_file, $lo_name, $xml->getLanguage());
+    } else {
         scorm_html5_page_create($row['template_framework'], $lo_name, $xml->getLanguage());
     }
-}else if ($scorm=="2004"){
+} else if ($scorm == "2004") {
     $useflash = ($export_flash && !$export_html5);
     lmsmanifest_2004_create($row['zipname'], $useflash, $lo_name);
-    if ($export_flash && !$export_html5)
-    {
-        scorm2004_html_page_create($row['template_name'],$row['template_framework'], $rlo_file, $lo_name, $xml->getLanguage());
-    }
-    else
-    {
+    if ($export_flash && !$export_html5) {
+        scorm2004_html_page_create($row['template_name'], $row['template_framework'], $rlo_file, $lo_name, $xml->getLanguage());
+    } else {
         scorm2004_html5_page_create($row['template_framework'], $lo_name, $xml->getLanguage());
     }
-}else{
-    if ($export_flash)
-    {
-        basic_html_page_create($row['template_name'],$row['template_framework'], $rlo_file, $lo_name);
+} else {
+    if ($export_flash) {
+        basic_html_page_create($row['template_name'], $row['template_framework'], $rlo_file, $lo_name);
     }
-    if ($export_html5)
-    {
+    if ($export_html5) {
         basic_html5_page_create($row['template_framework'], $lo_name);
     }
 }
@@ -285,11 +262,18 @@ if($scorm=="true"){
 
 xerte_zip_files($fullArchive, $dir_path);
 $zipfile->create_archive();
+
+
+// This outputs http headers etc.
 $zipfile->download_file($row['zipname']);
+
+_debug("Zip file errors? " . implode(',', $zipfile->error));
 
 /*
  * remove the files
  */
 clean_up_files();
-unlink($dir_path . "template.xml");
-unlink($dir_path . $zipfile_tmpname); 
+
+@unlink($dir_path . "template.xml");
+
+@unlink($zipfile_tmpname);
