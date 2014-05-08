98f7a18d8974a115a838d87dfb3e90f387c6f783
David Goodwin <david@palepurple.co.uk>
2014-05-08 14:15:59 +0100

    remove mysql_query calls (website_code/php/scorm/*.php) - see #29

--- website_code/php/scorm/export.php
+++ website_code/php/scorm/export.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * export, allows the creation of zip and scorm packages
@@ -8,49 +9,49 @@
  * @copyright Copyright (c) 2008,2009 University of Nottingham
  * @package
  */
-
 require_once("../../../config.php");
 include "../template_status.php";
-
+$prefix = $xerte_toolkits_site->database_table_prefix;
 
 ini_set('max_execution_time', 300);
 
-if(is_numeric($_GET['template_id'])){
+if (is_numeric($_GET['template_id'])) {
     $_GET['template_id'] = (int) $_GET['template_id'];
     $proceed = false;
-    if(is_template_exportable($_GET['template_id'])){
+    if (is_template_exportable($_GET['template_id'])) {
         $proceed = true;
-    }else{
-        if(is_user_creator($_GET['template_id'])||is_user_admin()){
+    } else {
+        if (is_user_creator($_GET['template_id']) || is_user_admin()) {
             $proceed = true;
         }
     }
 
-    if($proceed){
-	
-		$fullArchive = false;
-	
-        if (isset($_GET['full'])){
-            if($_GET['full']=="true"){
+    if ($proceed) {
+
+        $fullArchive = false;
+
+        if (isset($_GET['full'])) {
+            if ($_GET['full'] == "true") {
                 $fullArchive = true;
             }
-
         }
         _debug("Full archive: " . $fullArchive);
-        $mysql_id=database_connect("Scorm export database connect success","Scorm export database connect failed");
 
         /*
          * Get the file path
          */
-        $query = "select " . $xerte_toolkits_site->database_table_prefix . "templatedetails.template_name as zipname, " . $xerte_toolkits_site->database_table_prefix . "templaterights.template_id, " . $xerte_toolkits_site->database_table_prefix . "logindetails.username, " . $xerte_toolkits_site->database_table_prefix . "originaltemplatesdetails.template_name, " . $xerte_toolkits_site->database_table_prefix . "originaltemplatesdetails.template_framework from " . $xerte_toolkits_site->database_table_prefix . "templaterights, " . $xerte_toolkits_site->database_table_prefix . "logindetails, " . $xerte_toolkits_site->database_table_prefix . "originaltemplatesdetails, " . $xerte_toolkits_site->database_table_prefix . "templatedetails where " . $xerte_toolkits_site->database_table_prefix . "templatedetails.template_type_id = " . $xerte_toolkits_site->database_table_prefix . "originaltemplatesdetails.template_type_id and " . $xerte_toolkits_site->database_table_prefix . "templaterights.template_id = " . $xerte_toolkits_site->database_table_prefix . "templatedetails.template_id and " . $xerte_toolkits_site->database_table_prefix . "templatedetails.creator_id = " . $xerte_toolkits_site->database_table_prefix . "logindetails.login_id and " . $xerte_toolkits_site->database_table_prefix . "templaterights.template_id=\"" . mysql_real_escape_string($_GET['template_id']) . "\" AND role=\"creator\"";
-        $query_response = mysql_query($query);
-        $row = mysql_fetch_array($query_response);
-		
-		if(file_exists($xerte_toolkits_site->root_file_path . "modules/" . $row['template_framework'] . "/export.php")){
-		
-			require_once($xerte_toolkits_site->root_file_path . "modules/" . $row['template_framework'] . "/export.php");
-		
-		}
-		
+        $query = "select {$prefix}templatedetails.template_name as zipname, {$prefix}templaterights.template_id, "
+                . "{$prefix}logindetails.username, {$prefix}originaltemplatesdetails.template_name,"
+                . "{$prefix}originaltemplatesdetails.template_framework from {$prefix}templaterights, {$prefix}logindetails, "
+                . "{$prefix}originaltemplatesdetails, {$prefix}templatedetails WHERE "
+                . "{$prefix}templatedetails.template_type_id = {$prefix}originaltemplatesdetails.template_type_id and "
+                . "{$prefix}templaterights.template_id = {$prefix}templatedetails.template_id and "
+                . "{$prefix}templatedetails.creator_id = {$prefix}logindetails.login_id and {$prefix}templaterights.template_id= ? AND role= ?";
+
+        $params = array($_GET['template_id'], 'creator');
+        $row = db_query_one($query, $params);
+        if (file_exists($xerte_toolkits_site->root_file_path . "modules/" . $row['template_framework'] . "/export.php")) {
+            require_once($xerte_toolkits_site->root_file_path . "modules/" . $row['template_framework'] . "/export.php");
+        }
     }
 }
