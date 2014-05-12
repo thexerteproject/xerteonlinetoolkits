fe8c752c96b15a7c5daaa4afeba5e89674e0b6e1
David Goodwin <david@palepurple.co.uk>
2014-05-12 16:08:03 +0100

    See #29 - missing  (php/properties/access_change_template.php)

--- website_code/php/properties/access_change_template.php
+++ website_code/php/properties/access_change_template.php
@@ -45,6 +45,8 @@ $database_id = database_connect("Access change database connect success","Access
 /*
  * Update the database setting
  */
+$prefix = $xerte_toolkits_site->database_table_prefix;
+
  $query = "UPDATE {$prefix}templatedetails SET access_to_whom = ? WHERE template_id = ?";
 if(isset($_POST['server_string'])){
     $access_to_whom = $_POST['access'] . '-' . $_POST['server_string'];    
