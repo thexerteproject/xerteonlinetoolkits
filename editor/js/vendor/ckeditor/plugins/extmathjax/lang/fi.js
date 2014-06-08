a51eb70c0c55794a0d720dda31969b4dc56080ab
Tom Reijnders <reijnders@tor.nl>
2014-06-08 20:52:38 +0200

    Extended the ckeditor mathjax plugin and updated site icons
    - extmathjax also supports AsciiMATH and MathML, and toggling between
      inline and block view

---
+++ editor/js/vendor/ckeditor/plugins/extmathjax/lang/fi.js
@@ -0,0 +1,20 @@
+/*
+Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'mathjax', 'fi', {
+	title: 'Matematiikkaa TeX:llä',
+	button: 'Matematiikka',
+	dialogInput: 'Kirjoita TeX:iä tähän',
+	docUrl: 'http://en.wikibooks.org/wiki/LaTeX/Mathematics',
+	docLabel: 'TeX dokumentaatio',
+	loading: 'lataa...',
+	pathName: 'matematiikka',
+    MathJaxOptions: 'MathJax options',
+    mjsyntax: 'Choose syntax',
+    TeX: 'use TeX',
+    AsciiMath: 'use AsciiMath',
+    MathML: 'use MathML',
+    displayasblock: 'Display as block'
+
+} );
