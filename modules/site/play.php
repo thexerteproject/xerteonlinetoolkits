d5991f81b6c423ea889fc41707845a30efb43038
Tom Reijnders <reijnders@tor.nl>
2014-09-02 15:57:44 +0200

    Change path to MathJax CDN to protocol agnostic syntax, and modify site

--- modules/site/play.php
+++ modules/site/play.php
@@ -70,7 +70,7 @@ function show_template($row_play){
 
 		</script>
 
-		<script type="text/javascript" src="https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-MML-AM_HTMLorMML-full"></script>
+		<script type="text/javascript" src="//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_HTMLorMML-full"></script>
 
 	</head>
 
