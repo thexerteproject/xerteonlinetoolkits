954ba91c6d0a13458ab442fa1a88ec7618df88e1
Tom Reijnders <reijnders@tor.nl>
2014-10-10 21:35:21 +0200

    Add src/site, so the site template can be translated as well using the new xertetrans

--- build/rebuildNottingham.bat
+++ build/rebuildNottingham.bat
@@ -1,4 +1,4 @@
 @echo off
-.\php\php.exe ..\website_code\php\rebuildtemplate.php ..\src\Nottingham\wizards\en-GB > convert.log
+.\php\php.exe -c .\php ..\website_code\php\rebuildtemplate.php ..\src\Nottingham\wizards\en-GB > convert.log
 copy ..\src\Nottingham\wizards\en-GB\template.xwd ..\modules\xerte\parent_templates\Nottingham\wizards\en-GB\data.xwd
 
