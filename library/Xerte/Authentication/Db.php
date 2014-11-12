5ea05546b8bf19a9d25a8ebbbacc2001a266e1a5
Julian Tenney <julian.tenney@nottingham.ac.uk>
2014-11-12 14:48:05 +0000

    More license headers...

--- library/Xerte/Authentication/Db.php
+++ library/Xerte/Authentication/Db.php
@@ -1,5 +1,24 @@
 <?php
 /**
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+/**
  * Requires a db table called user which contains fields called firstname, surname, password and username.
  *
  * CREATE TABLE user ( id int(11) primary key auto_increment, username varchar(50) not null, password varchar(50) not null, firstname varchar(50) not null, surname varchar(50) not null) ;
