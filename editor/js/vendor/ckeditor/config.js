cabfdd9b1c41590fcf47ad93acc03538a012df73
Tom Reijnders <reijnders@tor.nl>
2015-03-01 20:11:16 +0100

    Right click -> mark word functionality added

--- editor/js/vendor/ckeditor/config.js
+++ editor/js/vendor/ckeditor/config.js
@@ -24,7 +24,7 @@ CKEDITOR.editorConfig = function( config ) {
         { name: 'others' }
     ];
 
-    config.extraPlugins = 'dialog,widget,extmathjax,image2,oembed';
+    config.extraPlugins = 'dialog,widget,extmathjax,image2,oembed,xotmarkword';
     // The default plugins included in the basic setup define some buttons that
     // we don't want too have in a basic editor. We remove them here.
     //config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
