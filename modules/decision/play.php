26b9c9bca9b86349c31edd2b9e1b5c17e8a25fa3
Tom Reijnders <reijnders@tor.nl>
2015-03-16 12:35:56 +0100

    Broke play and preview when implementing export, fixed.

--- modules/decision/play.php
+++ modules/decision/play.php
@@ -51,7 +51,7 @@ function show_template($row_play)
     _load_language_file("/modules/decision/preview.inc");
 
     // $engine is assumed to be html5 if flash is NOT set
-    $page_content = file_get_contents($xerte_toolkits_site->basic_template_path . $row_play['template_framework'] . "/player/rloObject.htm");
+    $page_content = file_get_contents($xerte_toolkits_site->basic_template_path . $row_play['template_framework'] . "/player_html5/rloObject.htm");
     $page_content = str_replace("%TITLE%", $title, $page_content);
     $page_content = str_replace("%TEMPLATEPATH%", $template_path_string, $page_content);
     $page_content = str_replace("%XMLPATH%", $string_for_flash, $page_content);
