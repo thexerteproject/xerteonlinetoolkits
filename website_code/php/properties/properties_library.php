d46cfed2180fb7899279b3d94e2fd2e06e58fe44
Tom Reijnders <reijnders@tor.nl>
2015-05-25 15:51:05 +0200

    Fix properties

--- website_code/php/properties/properties_library.php
+++ website_code/php/properties/properties_library.php
@@ -625,7 +625,7 @@ function sharing_info($template_id)
     $info =  "<p class=\"info_header\"><span>" . PROPERTIES_TAB_SHARED . "</span></p>";
 
     if(sizeof($query_sharing_rows)==1){
-        echo "<p class=\"share_files_paragraph\"><span>" . SHARING_NOT_SHARED . "</span</p>";
+        $info .= "<p class=\"share_files_paragraph\"><span>" . SHARING_NOT_SHARED . "</span></p>";
         exit(0);
     }
 
