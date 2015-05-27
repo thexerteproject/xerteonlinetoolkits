459ffcc3ea0dbad294703669e10250bc897651c3
Tom Reijnders <reijnders@tor.nl>
2015-05-27 16:57:35 +0200

    Fixed #291, introduced by removing site.rlt and decision.rlt

--- website_code/php/screen_size_library.php
+++ website_code/php/screen_size_library.php
@@ -34,24 +34,30 @@ function get_template_screen_size($filename, $type){
 
     $filename = $xerte_toolkits_site->basic_template_path . $type . "/parent_templates/" . $filename . "/" . $filename . ".rlt";
 
-    $data = file_get_contents($filename);
+    if (file_exists($filename))
+    {
+        $data = file_get_contents($filename);
+        $place = strpos($data, 'stageSize="')+11;
 
-    $place = strpos($data, 'stageSize="')+11;
+        if($place!=11){
 
-    if($place==11){
+            return "805~635";
 
+        }else{
+
+            $secondplace = strpos($data, '"', $place);
+
+            $temp = substr($data, $place, ($secondplace-$place));
+
+            $temp = split(",",$temp);
+
+            return $temp[0] . "~" . $temp[1];
+        }
+    }
+    else
+    {
         return "805~635";
-
-    }else{
-
-        $secondplace = strpos($data, '"', $place);
-
-        $temp = substr($data, $place, ($secondplace-$place));
-
-        $temp = split(",",$temp);
-
-        return $temp[0] . "~" . $temp[1];
-    }	
+    }
 
 }
 
