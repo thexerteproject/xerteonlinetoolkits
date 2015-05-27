01a8e4dfa5d5cb0e8bbd97f937bec5ae74f097ed
Tom Reijnders <reijnders@tor.nl>
2015-05-27 22:08:31 +0200

    Fixed #293

--- modules/xerte/engine/upload.php
+++ modules/xerte/engine/upload.php
@@ -35,11 +35,11 @@
 if ($_GET['BROWSER'] == 'firefox' || $_GET['BROWSER'] == 'safari') {
     if ($_GET['AUTH'] == 'moodle') {
         if (!isset($_COOKIE['MoodleSession']) || !isset($_COOKIE['MOODLEID1_'])) {
-            $temp = split('; ', $_GET['COOKIE']);
+            $temp = explode('; ', $_GET['COOKIE']);
             if (!empty($temp)) {
                 $cookie = array();
                 foreach ($temp as $key => $value) {
-                    $pair = split('=', $value);
+                    $pair = explode('=', $value);
                     $cookie[$pair[0]] = $pair[1];
                 }
                 $_COOKIE = $cookie; // We want to overwrite all
