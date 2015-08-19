a4ee3315f85bc44936da613f3d368ce4252a73cd
FayCross <cross.fay@gmail.com>
2015-08-19 11:02:29 +0100

    Fix #374 - glossary will now highlight words preceded & proceeded by tags and non-breaking spaces (&nbsp;), and proceeded by : or ;

--- modules/xerte/parent_templates/Nottingham/common_html5/js/xenith.js
+++ modules/xerte/parent_templates/Nottingham/common_html5/js/xenith.js
@@ -1454,11 +1454,11 @@ function x_insertText(node) {
     // check text for glossary words - if found replace with a link
     if (x_glossary.length > 0) {
         for (var k=0, len=x_glossary.length; k<len; k++) {
-            var regExp = new RegExp('(^|\\s)(' + x_glossary[k].word + ')([\\s\\.,!?]|$)', 'i');
+			var regExp = new RegExp('(^|[\\s>]|&nbsp;)(' + x_glossary[k].word + ')([\\s\\.,!?:;<]|$|&nbsp;)', 'i');
 			tempText = tempText.replace(regExp, '$1{|{'+k+'::$2}|}$3');
         }
         for (var k=0, len=x_glossary.length; k<len; k++) {
-			var regExp = new RegExp('(^|\\s)(\\{\\|\\{' + k + '::(.*?)\\}\\|\\})([\\s\\.,!?]|$)', 'i');
+			var regExp = new RegExp('(^|[\\s>]|&nbsp;)(\\{\\|\\{' + k + '::(.*?)\\}\\|\\})([\\s\\.,!?:;<]|$|&nbsp;)', 'i');
 			//tempText = tempText.replace(regExp, '$1<a class="x_glossary" href="#" title="' + x_glossary[k].definition + '">$3</a>$4');
 			tempText = tempText.replace(regExp, '$1<a class="x_glossary" href="#" def="' + x_glossary[k].definition.replace(/\"/g, "'") + '">$3</a>$4');
         }
