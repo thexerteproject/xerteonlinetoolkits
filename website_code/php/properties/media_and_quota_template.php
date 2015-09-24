0b6fc54642d1c6c6387a71da205e35ca8529961b
Julian Tenney <julian.tenney@nottingham.ac.uk>
2015-09-24 10:45:25 +0100

    Looks like this fixes the issue with spaces in file names and media and quota.

--- website_code/php/properties/media_and_quota_template.php
+++ website_code/php/properties/media_and_quota_template.php
@@ -53,6 +53,7 @@ function in_use($file_name){
 
     global $xmlpath, $previewpath;
 	$file_name2 = str_replace("&", "&amp;", $file_name);
+	$file_name2 = str_replace(" ", "%20", $file_name);
     if(!strpos(file_get_contents($xmlpath),$file_name)&&!strpos(file_get_contents($previewpath),$file_name)&&!strpos(file_get_contents($xmlpath),$file_name2)&&!strpos(file_get_contents($previewpath),$file_name2)){
         return false;
     }else{
