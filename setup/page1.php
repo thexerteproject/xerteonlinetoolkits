8e3d44c1119275e8aa80ba3c73fce35881056db0
Leonard Johnson (Len) <radixmo@gmail.com>
2015-10-11 21:35:23 +0100

    Minor updates and fixes
    - changed uppercase php tags to lowercase
    - removed empty lines from license header
    - added new header and footer files

--- setup/page1.php
+++ setup/page1.php
@@ -3,7 +3,7 @@
  * Licensed to The Apereo Foundation under one or more contributor license
  * agreements. See the NOTICE file distributed with this work for
  * additional information regarding copyright ownership.
-
+ *
  * The Apereo Foundation licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at:
@@ -13,11 +13,11 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-
+ *
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- echo file_get_contents("page_top");
+require_once('page_header.php');
 if ( extension_loaded('pdo')  && extension_loaded('pdo_mysql')) { // e.g., pdo_mysql
 
 ?>
@@ -55,6 +55,4 @@ On this page the installer will execute a database query to create the database 
 
 ?>
 
-
-</body>
-</html>
+<?php require_once('page_footer.php'); ?>
