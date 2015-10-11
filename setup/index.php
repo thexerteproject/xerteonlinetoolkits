2db3cbed4c2a8ed93ec823645d321315546253c9
Leonard Johnson (Len) <radixmo@gmail.com>
2015-10-11 19:14:46 +0100

    Updated index.php (see description for details)
    - now uses new header and footer files
    - wording of page changed and re-organised to include an about section
    and a notes section
    - XAMPP button removed (users now only have one button to install)

--- setup/index.php
+++ setup/index.php
@@ -3,7 +3,7 @@
  * Licensed to The Apereo Foundation under one or more contributor license
  * agreements. See the NOTICE file distributed with this work for
  * additional information regarding copyright ownership.
-
+ *
  * The Apereo Foundation licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at:
@@ -13,36 +13,47 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-
+ *
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-echo file_get_contents("page_top");
+require_once('page_header.php'); 
 
-?>
-<h2 style="margin-top:15px">
-Welcome to the Xerte Online Toolkits Installer
-</h2>
-<?PHP
+if ( file_exists( '../database.php' ) ):
 
-	if(file_exists("../database.php")){
+    require_once('software_installed.php');
 
-		die("<p>You appear to have already installed toolkits</p><p>Please go to <a href='http://" . $_SERVER['HTTP_HOST'] . str_replace("setup/", "", $_SERVER['PHP_SELF']) . "'>Xerte Online Toolkits Install</a></p>");
+else: ?>
 
-	}
+			<h2>About</h2>
 
-?>
-<p>
-Xerte Online Toolkits is a suite of web based tools designed and developed by a wonderful community of <a href="http://www.xerte.org.uk" target="_blank">open source developers.</a></p>
-<p>Xerte Online Toolkits is a powerful suite of browser-based tools that allow anyone with a web browser to log on and create interactive learning materials simply and effectively, and to collaborate with other users in developing content. Xerte Online Toolkits provides a number of project templates for creating online presentations and interactive content. Content is assembled using an intuitive interface, and multiple users can collaborate on shared projects. Xerte Toolkits is free software, released under the Apache License v2.0:
-</p>
-<p>
-<p>
-	<form action="xampp.php"><button type="submit">Press here for XAMPP People</button></form>
-</p>
-<p>
-	<form action="config_setup.php"><button type="submit">Press here for a full install</button></form>
-</p>
-<p><b>Please note:</b> If you install locally and use XAMPP it may not run if you are using Skype. Please disable Skype if you intend to use XAMPP locally.</p>
-</body>
-</html>
+			<p>Xerte Online Toolkits is a suite of web based tools designed and developed by a wonderful community of <a href="http://www.xerte.org.uk" target="_blank">open source developers.</a></p>
+
+			<p>Xerte Online Toolkits is a powerful suite of browser-based tools that allow anyone with a web browser to log on and create interactive learning materials simply and effectively, and to collaborate with other users in developing content. Xerte Online Toolkits provides a number of project templates for creating online presentations and interactive content. Content is assembled using an intuitive interface, and multiple users can collaborate on shared projects. Xerte Toolkits is free software, released under the Apache License v2.0.</p>
+
+			<h2>Notes</h2>
+
+			<ul>
+	    	<li>Xerte Online Toolkits supports multiple authentication types (Database, Moodle, LDAP or a Static list). </li>
+				
+				<li>Although not critical, Xerte Online Toolkits uses PHP mail functions for parts of its code. You can remove these manually from the code should you so wish.</li>
+
+	    	<li>At present, with files positioned as they are:
+	    		<ul>
+	    			<li>your system will be installed at <code><?php echo $xot_setup->getRootPath(); ?></code></li>
+	    			<li>the web address for your system will be: <code><?php echo $xot_setup->getXotUrl(); ?></code></li>
+	    		</ul>
+	    	</li>
+
+				<li><strong>Please note:</strong> If you install locally and use XAMPP, the software may not run if you are using Skype. Please disable Skype if you intend to use XAMPP locally.</li>
+    	</ul>
+
+			<h2>Begin Installation</h2>
+
+    	<p>The next pages will help you verify and solve system issues. You will not be able to continue until all requirements are fulfilled.</p>
+
+			<a href="requirements.php"><button>Press here to install</button></a>
+
+<?php require_once('page_footer.php'); ?>
+
+<?php endif; ?>
