449b9fb5de66c110bbf1702d6a617164df3ae78c
Leonard Johnson (Len) <radixmo@gmail.com>
2015-10-11 21:36:41 +0100

    Minor updates and fixes
    - removed empty lines from license header
    - added new header and footer files

--- setup/page2.php
+++ setup/page2.php
@@ -3,7 +3,7 @@
  * Licensed to The Apereo Foundation under one or more contributor license
  * agreements. See the NOTICE file distributed with this work for
  * additional information regarding copyright ownership.
-
+ *
  * The Apereo Foundation licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at:
@@ -13,7 +13,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-
+ *
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -26,7 +26,7 @@ function _debug($string) {
     $dberr = $string;
 }
 
-echo file_get_contents("page_top");
+require_once('page_header.php');
 if (!isset($_POST['database_created']))
 {
 
@@ -194,6 +194,4 @@ else
 <?php
 }
 ?>
-</div>
-</body>
-</html>
+<?php require_once('page_footer.php'); ?>
