901aa670e8f9b31fc2c87c00d7ed9b723f861183
Tom Reijnders <reijnders@tor.nl>
2015-11-14 21:45:41 +0100

    Use new way of setting tooltips and add PIN and UNPIN

--- languages/en-GB/website_code/scripts/screen_display.js
+++ languages/en-GB/website_code/scripts/screen_display.js
@@ -7,8 +7,12 @@
 	 * @package
 	 */
 
-	var OPENTOOLTIP = "Open this pane";
+	var LAYOUT_OPENTOOLTIP = "Open this pane";
 
-	var CLOSETOOLTIP = "Close this pane";
+	var LAYOUT_CLOSETOOLTIP = "Close this pane";
 
-	var RESIZETOOLTIP = "Resize this pane";
+	var LAYOUT_RESIZETOOLTIP = "Resize this pane";
+
+	var LAYOUT_UNPIN = "Un-Pin";
+
+	var LAYOUT_PIN = "Pin";
