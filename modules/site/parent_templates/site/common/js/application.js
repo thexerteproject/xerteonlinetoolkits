d0f7f603ac64f906f577e2b68e9ac72c542c7437
FayCross <cross.fay@gmail.com>
2017-04-27 09:18:15 +0100

    Fixed typo in previous commit

--- modules/site/parent_templates/site/common/js/application.js
+++ modules/site/parent_templates/site/common/js/application.js
@@ -383,7 +383,7 @@ function parseContent(pageIndex){
 				$link.find('a').append(tocName);
 				
 				//add the section header
-				var msg = languageData.find("hiddenPage")[0] != undefined && languageData.find("hiddenSection")[0].getAttribute('label') != null ? languageData.find("hiddenSection")[0].getAttribute('label') : "This section will be hidden in live projects";
+				var msg = languageData.find("hiddenSection")[0] != undefined && languageData.find("hiddenSection")[0].getAttribute('label') != null ? languageData.find("hiddenSection")[0].getAttribute('label') : "This section will be hidden in live projects";
 				var extraTitle = $(this).attr('hidePage') == 'true' ? '<p class="alertMsg">' + msg + '</p>' : '';
 				
 				var links = '';
