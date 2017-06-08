bb1a36257b45ae134213aaa6996ad2895c353b68
FayCross <cross.fay@gmail.com>
2017-06-08 11:11:39 +0100

    Undo fix #601

--- editor/js/toolbox.js
+++ editor/js/toolbox.js
@@ -1238,7 +1238,7 @@ var EDITOR = (function ($, parent) {
                     [
                         [ 'Font', 'FontSize', 'TextColor', 'BGColor' ],
                         [ 'Bold', 'Italic', 'Underline', 'Superscript', 'Subscript'],
-						[ 'JustifyLeft', 'JustifyCenter', 'JustifyRight' ],
+						//[ 'JustifyLeft', 'JustifyCenter', 'JustifyRight' ],
                         [ 'Sourcedialog' ],
                         [ 'FontAwesome']
                     ],
@@ -2582,13 +2582,11 @@ var EDITOR = (function ($, parent) {
                     form_id_offset++;
                     if (options.wysiwyg && options.wysiwyg!="false")
                     {
-                        var htmlRegex = new RegExp(/<[a-z/][\s\S]*>/i);
-						
                         html = $('<div>')
                             .attr('id', id)
                             .addClass('inlinewysiwyg')
                             .attr('contenteditable', 'true')
-							.append(htmlRegex.test(value) == true ? ( value.substring(0,2) == "<p" ? value : '<p>' + value + '</p>' ) : '<p>' + value + '</p>');
+							.append('<p>' + value + '</p>');
 						
                         textinputs_options.push({id: id, key: key, name: name, options: options});
                     }
