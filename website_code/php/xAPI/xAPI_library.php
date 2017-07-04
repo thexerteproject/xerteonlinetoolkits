1bb46f11ab3f39fb0b8345dd4123aa162da5bb7a
Joris Burgers <jorisburgers@live.nl>
2017-07-04 17:48:25 +0200

    Added support for Tsugi in Xerte

--- website_code/php/xAPI/xAPI_library.php
+++ website_code/php/xAPI/xAPI_library.php
@@ -1,13 +1,17 @@
 <?php
 
-function xAPI_html_page_create($template_name, $type, $lo_name, $language) {
+function xAPI_html_page_create($template_name, $type, $lo_name, $language, $tsugi) {
 
 global $xerte_toolkits_site, $dir_path, $delete_file_array, $zipfile, $youtube_api_key;
 
 	$version = file_get_contents(dirname(__FILE__) . "/../../../version.txt");
+	if($tsugi)
+	{
+		$xapi_html_page_content = file_get_contents($xerte_toolkits_site->basic_template_path . $type . "/player_html5/rloObject.php");
+	}else{
+		$xapi_html_page_content = file_get_contents($xerte_toolkits_site->basic_template_path . $type . "/player_html5/rloObject.htm");
 
-	$xapi_html_page_content = file_get_contents($xerte_toolkits_site->basic_template_path . $type . "/player_html5/rloObject.htm");
-
+	}
 	$xapi_html_page_content = str_replace("%VERSION%", $version , $xapi_html_page_content);
     $xapi_html_page_content = str_replace("%VERSION_PARAM%", "", $xapi_html_page_content);
     $xapi_html_page_content = str_replace("%TITLE%",$lo_name,$xapi_html_page_content);
@@ -34,15 +38,20 @@ global $xerte_toolkits_site, $dir_path, $delete_file_array, $zipfile, $youtube_a
 	$xapi_html_page_content = str_replace("%TRACKING_SUPPORT%",$tracking,$xapi_html_page_content);
 	$xapi_html_page_content = str_replace("%YOUTUBEAPIKEY%", $youtube_api_key, $xapi_html_page_content);
 	
-
-	$file_handle = fopen($dir_path . "index.htm", 'w');
+	if($tsugi)
+	{
+		$index_file = "index.php";
+	}else{
+		$index_file = "index.htm";
+	}
+	$file_handle = fopen($dir_path . $index_file, 'w');
 	
 	fwrite($file_handle,$xapi_html_page_content,strlen($xapi_html_page_content));
 	fclose($file_handle);
 	
-	$zipfile->add_files("index.htm");
+	$zipfile->add_files($index_file);
 	
-	array_push($delete_file_array,  $dir_path . "index.htm");
+	array_push($delete_file_array,  $dir_path . $index_file);
 	
 	
 	/*
