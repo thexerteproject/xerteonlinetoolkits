f67e4cc1cf04dc9cef87a7aeb48b7e0d1f87fadd
Joris Burgers <jorisburgers@live.nl>
2017-07-18 14:59:51 +0200

    Added fix for empty credentials in xAPI

--- website_code/php/xAPI/xAPI_library.php
+++ website_code/php/xAPI/xAPI_library.php
@@ -31,7 +31,9 @@ global $xerte_toolkits_site, $dir_path, $delete_file_array, $zipfile, $youtube_a
 	$tracking = "<script type=\"text/javascript\" src=\"xttracking_xapi.js\"></script>\n";
 	$tracking .= "<script type=\"text/javascript\" src=\"languages/js/en-GB/xttracking_xapi.js\"></script>\n";
 	$tracking .= "<script type=\"text/javascript\" src=\"tincan.js\"></script>\n";
-	$tracking .= "<script>var lrsEndpoint=\"$endpoint\";var lrsPassword=\"$secret\"; var lrsUsername=\"$key\";</script>";
+	if(!$tsugi){
+		$tracking .= "<script>var lrsEndpoint=\"$endpoint\";var lrsPassword=\"$secret\"; var lrsUsername=\"$key\";</script>";
+	}
 	if (file_exists($dir_path . "languages/js/" . $language . "/xttracking_xapi.js") && $language != "en-GB")
 	{
 		$tracking .= "<script type=\"text/javascript\" src=\"languages/js/" . $language . "/xttracking_xapi.js\"></script>";
