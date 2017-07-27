31a8139d898e5e7525c39e7c465183f6c54f3b20
Smith, John <J.J.Smith@gcu.ac.uk>
2017-07-27 15:27:47 +0100

    Fix path in Bootstrap rebuild script

--- build/rebuildsite.sh
+++ build/rebuildsite.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 php ../website_code/php/rebuildtemplate.php ../src/site/wizards/en-GB > convert.log
-cp ../src/site/wizards/en-GB/template.xwd ../modules/xerte/parent_templates/site/wizards/en-GB/data.xwd
+cp ../src/site/wizards/en-GB/template.xwd ../modules/site/parent_templates/site/wizards/en-GB/data.xwd
