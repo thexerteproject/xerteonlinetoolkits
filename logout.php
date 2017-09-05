1796f0153ad698f4ae11fe20f5430ca798aefab9
John Horne <john.horne@plymouth.ac.uk>
2017-09-05 14:14:15 +0100

    Ensure logouts get logged.

--- logout.php
+++ logout.php
@@ -19,6 +19,23 @@
  */
 require_once("config.php");
 
+if (isset($_SESSION['toolkits_logon_id'])) {
+    if ($_SESSION['toolkits_logon_id'] == "site_administrator") {
+        $msg = "Admin user (from " . $_SERVER['REMOTE_ADDR'] . ") has logged out";
+        receive_message("", "SYSTEM", "MGMT", "Logout", $msg);
+    }
+    else {
+        if (isset($_SESSION['toolkits_logon_username'])) {
+            $msg = "User " . $_SESSION['toolkits_logon_username'] . " (from " . $_SERVER['REMOTE_ADDR'] . ") has logged out";
+        }
+        else {
+            $msg = "Unknown user (from " . $_SERVER['REMOTE_ADDR'] . ") has logged out";
+        }
+
+        receive_message("", "SYSTEM", "LOGINS", "Logout", $msg);
+    }
+}
+
 $authmech = Xerte_Authentication_Factory::create($xerte_toolkits_site->authentication_method);
 
 if ($authmech->hasLogout())
