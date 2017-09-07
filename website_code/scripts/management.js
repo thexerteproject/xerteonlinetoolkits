9b4b698dd168b6f1d6bda0dbb1e386002db1bc7b
John Horne <john.horne@plymouth.ac.uk>
2017-09-07 15:27:11 +0100

    Put encodingURI call inline with getting the value. Removed previous code, as not required.

--- website_code/scripts/management.js
+++ website_code/scripts/management.js
@@ -399,12 +399,6 @@ function update_site(){
 		copyright = document.getElementById("copyright").value;
 		copyright = copyright.split("�").join("AAA");
 
-		// Some elements, such as passwords, may contain HTML-interpreted characters,
-		// such as '&'. These elements need to be encoded before being POST'd.
-
-		var bpwd = document.getElementById("bind_pwd").value;
-		bpwd = encodeURIComponent(bpwd);
-
 		xmlHttp.send('site_url=' + document.getElementById("site_url").value + 
 					 '&apache=' + document.getElementById("apache").value + 
 					 '&site_title=' + document.getElementById("site_title").value + 
@@ -435,7 +429,7 @@ function update_site(){
 					 '&authentication_method=' + document.getElementById("authentication_method").value + 
 					 '&ldap_host=' + document.getElementById("ldap_host").value	+ 
 					 '&ldap_port=' + document.getElementById("ldap_port").value + 
-					 '&bind_pwd=' + bpwd + 
+					 '&bind_pwd=' + encodeURIComponent(document.getElementById("bind_pwd").value) +
 					 '&base_dn=' + document.getElementById("base_dn").value + 
 					 '&bind_dn=' + document.getElementById("bind_dn").value + 
 					 '&flash_save_path=' + document.getElementById("flash_save_path").value + 
