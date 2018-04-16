15195324ad29487698d9f6ad43257cee8380635b
Tom Reijnders <reijnders@tor.nl>
2018-04-16 08:32:54 +0200

    Fixes and enhancements for opinion

--- themes/Nottingham/mobilewhite/mobilewhite.js
+++
@@ -1,2 +0,0 @@
-// use this file for any theme specific javascript/jQuery
-// e.g. if your css needs the interface to include extra tags then you can add them to your project from javascript in this file
