c55678ad8bfdba65b67c940439fb5ee73605c258
Ron Mitchell <ronm123@gmail.com>
2018-12-05 17:47:56 +0000

    Further tweaks to cardiff theme

--- themes/Nottingham/cesicardiff/cesicardiff.js
+++ themes/Nottingham/cesicardiff/cesicardiff.js
@@ -1,6 +1,7 @@
 /*var linkEl;*/
 
-/*$(document).ready(function(){
+$(document).ready(function(){
+    /*
     linkEl = $('<link rel="stylesheet" href="https://learningcentral.cf.ac.uk/bbcswebdav/courses/REGOS-ELTTStaff/XERTE%20Files/XT%20Toolkit%20Open.css" type="text/css">').appendTo('head')[0];
     $(window).trigger('resize');
 
@@ -42,7 +43,7 @@
 
     });
 
-
+*/
     $('#x_pageDiv').on('DOMNodeInserted', '.splitScreen', function(){
         $('.splitScreen:first').closest('#pageContents').addClass('withChild');
     });
@@ -58,7 +59,7 @@
 
 
 
-});*/
+});
 
 
 /*$(function(){
