8eca0b47dc29ba3f8b8db6b8cdb196903c259e80
JohnSmith_LT <jjs@gcu.ac.uk>
2019-11-17 23:52:48 +0000

    Add audio insert button to CKEditor for issue #447

---
+++ editor/js/vendor/ckeditor/plugins/html5audio/lang/de.js
@@ -0,0 +1,15 @@
+CKEDITOR.plugins.setLang( 'html5audio', 'de', {
+    button: 'HTML5 Audio einfügen',
+    title: 'HTML5 Audio',
+    infoLabel: 'Audio Infos',
+    urlMissing: 'Sie haben keine URL zur Audio-Datei angegeben.',
+    audioProperties: 'Audio-Einstellungen',
+    upload: 'Hochladen',
+    btnUpload: 'Zum Server senden',
+    advanced: 'Erweitert',
+    autoplay: 'Autoplay?',
+    allowdownload: 'Download zulassen?',
+    advisorytitle: 'Titel Beschreibung',
+    yes: 'Ja',
+    no: 'Nein'
+} );
