1af4da3059fca472a8eede95ea853a3263abc4e2
Tom Reijnders <t.reijnders@integrat-ed.nl>
2019-12-17 12:22:33 +0100

    Fix basic.sql and add column dashboard_display_options to templatedetails

--- setup/basic.sql
+++ setup/basic.sql
@@ -209,13 +209,14 @@ CREATE TABLE `$templatedetails` (
   `tsugi_xapi_secret` text,
   `tsugi_xapi_student_id_mode` int DEFAULT 0,
   `dashboard_allowed_links` text,
+  `dashboard_display_options` text,
   PRIMARY KEY (`template_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `$templaterights` (
   `template_id` bigint(20) NOT NULL,
   `user_id` bigint(20) DEFAULT NULL,
-  `role` text,
+  `role` char(255) DEFAULT NULL,
   `folder` bigint(20) DEFAULT NULL,
   `notes` char(255) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
