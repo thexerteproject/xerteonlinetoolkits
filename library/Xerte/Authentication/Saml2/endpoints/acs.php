2f950191eda04ecb0ae1eef62ad9e06810317e98
Tom Reijnders <reijnders@tor.nl>
2020-02-05 18:45:56 +0100

    Add updated Saml2 SP implementation

---
+++ library/Xerte/Authentication/Saml2/endpoints/acs.php
@@ -0,0 +1,60 @@
+<?php
+ 
+/**
+ *  SP Assertion Consumer Service Endpoint
+ */
+
+session_start();
+
+require_once dirname(__FILE__).'/../vendor/autoload.php';
+require_once dirname(__FILE__).'/../settings.php';
+
+use OneLogin\Saml2\Auth;
+use OneLogin\Saml2\Utils;
+use OneLogin\Saml2\IdPMetadataParser;
+
+$parseIdp = new IdPMetadataParser();
+$idpSettings = $parseIdp->parseRemoteXML($idpMetadataUrl);
+$settingsInfoArray = $parseIdp->injectIntoSettings($settingsInfoArray, $idpSettings);
+
+$auth = new Auth($settingsInfoArray);
+
+$auth->processResponse();
+
+$errors = $auth->getErrors();
+
+if (!empty($errors)) {
+    echo '<p>' . implode(', ', $errors) . '</p>';
+    exit();
+}
+
+if (!$auth->isAuthenticated()) {
+    echo '<p>Not authenticated</p>';
+    exit();
+}
+
+$_SESSION['samlUserdata'] = $auth->getAttributes();
+$_SESSION['IdPSessionIndex'] = $auth->getSessionIndex();
+if (isset($_POST['RelayState']) && Utils::getSelfURL() != $_POST['RelayState']) {
+    $auth->redirectTo($_POST['RelayState']);
+}
+
+$attributes = $_SESSION['samlUserdata'];
+
+if (!empty($attributes)) {
+    echo '<h1>'._('User attributes:').'</h1>';
+    echo '<table><thead><th>'._('Name').'</th><th>'._('Values').'</th></thead><tbody>';
+    foreach ($attributes as $attributeName => $attributeValues) {
+        echo '<tr><td>'.htmlentities($attributeName).'</td><td><ul>';
+        foreach ($attributeValues as $attributeValue) {
+            echo '<li>'.htmlentities($attributeValue).'</li>';
+        }
+        echo '</ul></td></tr>';
+    }
+    echo '</tbody></table>';
+    if (!empty($_SESSION['IdPSessionIndex'])) {
+        echo '<p>The SessionIndex of the IdP is: '.$_SESSION['IdPSessionIndex'].'</p>';
+    }
+} else {
+    echo _('Attributes not found');
+}
