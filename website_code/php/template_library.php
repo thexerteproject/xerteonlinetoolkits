7797c5869e3334c00f183c7078375b7893867763
Tom Reijnders <reijnders@tor.nl>
2020-06-12 19:49:14 +0200

    Introduce catalog function to query xerte installation

--- website_code/php/template_library.php
+++ website_code/php/template_library.php
@@ -148,3 +148,48 @@ function get_default_engine($template_id)
     }
 }
 
+function get_template_data_as_xmlstring($template_id, $creator_user_name="", $template_type_name="")
+{
+    global $xerte_toolkits_site;
+
+    // Check parameters
+    if ($creator_user_name == "" || $template_type_name == "") {
+        $prefix = $xerte_toolkits_site->database_table_prefix;
+        $q = "SELECT ld.username, otd.template_name FROM {$prefix}templatedetails td, {$prefix}originaltemplatesdetails, {$prefix}logindetails ld WHERE td.template_id = ? and td.creator_id=ld.login_id and td.template_type_id=otd.template_type_id";
+        $params = array($template_id);
+
+        $row = db_query_one($q, $params);
+
+        if ($row == false) {
+            receive_message($_SESSION['toolkits_logon_username'], "ADMIN", "CRITICAL", "Failed to get data as xml", "Failed to get the data as xml");
+            return "";
+        } else {
+            $creator_user_name = $row['username'];
+            $template_type_name = $row['template_name'];
+        }
+    }
+
+    // Construct file name
+    $template_dir = $xerte_toolkits_site->users_file_area_full . $template_id . "-" . $creator_user_name . "-" . $template_type_name . "/";
+    $dataFilename = $template_dir . "data.xml";
+
+    if (file_exists($dataFilename)) {
+
+        $data = file_get_contents($dataFilename);
+    } else {
+        $data = "";
+    }
+    return $data;
+}
+
+function get_template_data_as_xml($template_id, $creator_user_name="", $template_type_name="")
+{
+    $xml = "";
+    $dataXml = get_template_data_as_xmlstring($template_id, $creator_user_name, $template_type_name);
+    if (strlen($dataXml) > 0)
+    {
+        // Convert to JSON
+        $xml = simplexml_load_string($dataXml);
+    }
+    return $xml;
+}
