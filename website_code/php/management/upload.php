496e90af3ef36a2ebe3887c4e894ce7e39f9d523
Tom Reijnders <reijnders@tor.nl>
2020-08-12 18:47:09 +0200

    Oops upload of sub templates was broken by security patch

--- website_code/php/management/upload.php
+++ website_code/php/management/upload.php
@@ -17,7 +17,7 @@ _load_language_file("/website_code/php/management/upload.inc");
 global $xerte_toolkits_site;
 $prefix = $xerte_toolkits_site->database_table_prefix;
 
-if (!isset($_SESSION['toolkits_logon_username']))
+if (!is_user_admin())
 {
     _debug("Session is invalid or expired");
     die("Session is invalid or expired");
