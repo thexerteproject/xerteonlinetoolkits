9ef502aace3ccbb39afe7cebb21da11df439586d
Ron Mitchell <ronm123@gmail.com>
2020-10-06 13:12:27 +0100

    hide wcag logo by default to prevent flash of logo on load/refresh

--- modules/site/parent_templates/site/common/js/application.js
+++ modules/site/parent_templates/site/common/js/application.js
@@ -1111,6 +1111,7 @@ function setup() {
 		}
 
 		//populate wcag logo and link and/or hide it
+		$(".wcagLink").removeClass("hidden");
 		if ($(data).find('learningObject').attr('wcagAlt') != undefined){
 			$(".wcagLogo").attr("alt",$(data).find('learningObject').attr('wcagAlt'));
 		}
