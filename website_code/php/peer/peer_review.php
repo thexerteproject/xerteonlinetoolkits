6141a65b1b70ce3d0977a3e6eb73e440f324bae7
Ron Mitchell <ronm123@gmail.com>
2021-03-24 15:38:46 +0000

    Changes to peer review to detect if project is public or not as well as detect if apache is true or not. If public include play link if private include peer link. Use long or short form url depending on apache status. Need to add additional code or copy of this for feedback.php otherwise for non public projects the included link is to peer.

--- website_code/php/peer/peer_review.php
+++ website_code/php/peer/peer_review.php
@@ -9,7 +9,7 @@
  * compliance with the License. You may obtain a copy of the License at:
  *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,8 +18,8 @@
  * limitations under the License.
  */
 /**
- * 
- * peer view page, sends the email back to the 
+ *
+ * peer view page, sends the email back to the
  *
  * @author Patrick Lockley
  * @version 1.0
@@ -38,13 +38,31 @@ $query_for_file_name = "select template_name from {$xerte_toolkits_site->databas
 
 $row_template_name = db_query_one($query_for_file_name, array($_POST['template_id']));
 
+$query_for_access_to_whom = "select access_to_whom from {$xerte_toolkits_site->database_table_prefix}templatedetails where template_id =?";
+
+$row_access_to_whom = db_query_one($query_for_access_to_whom, array($_POST['template_id']));
+$access=$row_access_to_whom["access_to_whom"];
+
 $headers = get_email_headers();
 
 if(isset($_POST['retouremail'])){
 
-	$subject = PEER_REVIEW_FEEDBACK . " - \"" . str_replace("_"," ",$row_template_name['template_name']) ."\"";
-	
-	$message = PEER_REVIEW_EMAIL_GREETING . " <br><br> " . PEER_REVIEW_EMAIL_INTRO . "<br><br><br><a href='" . $xerte_toolkits_site->site_url . "/play.php?template_id=" . $_POST['template_id'] . "'>" . $xerte_toolkits_site->site_url . "/play.php?template_id=" . $_POST['template_id'] . "</a><br><br><br>" . str_replace("\n", "<br>\n", $_POST['feedback']) . "<br><br><br>" . PEER_REVIEW_EMAIL_YOURS . "<br><br>" . PEER_REVIEW_EMAIL_SIGNATURE;
+    if($xerte_toolkits_site->apache=="true") {
+        $playstring = "peerreview_";
+        if($row_access_to_whom["access_to_whom"]=="Public"){
+            $playstring = "play_";
+        }
+    }else{
+        $playstring = "peer.php?template_id=";
+        if($row_access_to_whom["access_to_whom"]=="Public"){
+            $playstring = "play.php?template_id=";
+        }
+    }
+
+
+    $subject = PEER_REVIEW_FEEDBACK . " - \"" . str_replace("_"," ",$row_template_name['template_name']) ."\"";
+
+    $message = PEER_REVIEW_EMAIL_GREETING . " <br><br> " . PEER_REVIEW_EMAIL_INTRO . " ". str_replace("_"," ",$row_template_name['template_name']) ."."."<br><br><br><a href='" . $xerte_toolkits_site->site_url . $playstring . $_POST['template_id'] . "'>" . $xerte_toolkits_site->site_url . $playstring . $_POST['template_id'] . "</a><br><br><br>" . str_replace("\n", "<br>\n", $_POST['feedback']) . "<br><br><br>" . PEER_REVIEW_EMAIL_YOURS . "<br><br>" . PEER_REVIEW_EMAIL_SIGNATURE;
 
     if(mail( $_POST['retouremail'], $subject, $message, $headers)){
 
