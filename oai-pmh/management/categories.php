d902daf033d3d67e7535c1c83fb066b4e8c3cfb4
Tom Reijnders <reijnders@tor.nl>
2021-11-23 16:16:05 +0100

    Fixes to set categories based on URL's

--- oai-pmh/management/categories.php
+++ oai-pmh/management/categories.php
@@ -1,24 +1,25 @@
 <?php
 
-// php categories.php "./vocabularies/opleidingsdomeinen.xml" "./vocabularies/domeinoverstijgend.xml"
+// php categories.php https://vdex.kennisnet.nl/kennisnetset/2015.01/mbo_opleidingsdomeinen_studierichtingen-knset.xml https://vdex.kennisnet.nl/kennisnetset/2015.01/bve_domeinoverstijgende_vakken-knset.xml
 
 require_once('../../config.php');
 
 
-call_user_func(clearCategoryTable);
-call_user_func(createCategoryTable);
 if ($argc > 1) {
+    clearCategoryTable();
+    createCategoryTable();
 
     for ($args = 1; $args < $argc; $args++) {
 
         $xmlfile = $argv[$args];
-        if($xmlfile == "./vocabularies/opleidingsdomeinen.xml"){
-            $source_url = "https://vdex.kennisnet.nl/kennisnetset/2015.01/mbo_opleidingsdomeinen_studierichtingen-knset.xml";
-        }
-        else {
-            $source_url = "https://vdex.kennisnet.nl/kennisnetset/2015.01/bve_domeinoverstijgende_vakken-knset.xml";
-        }
-        echo $source_url;
+        //if($xmlfile == "./vocabularies/opleidingsdomeinen.xml"){
+        //    $source_url = "https://vdex.kennisnet.nl/kennisnetset/2015.01/mbo_opleidingsdomeinen_studierichtingen-knset.xml";
+        //}
+        //else {
+        //    $source_url = "https://vdex.kennisnet.nl/kennisnetset/2015.01/bve_domeinoverstijgende_vakken-knset.xml";
+        //}
+        $source_url = $xmlfile;
+        echo $source_url . "\n";
         $xml = simplexml_load_file($xmlfile);
         $nodes = $xml->xpath("//vdex:term");
 
@@ -28,13 +29,15 @@ if ($argc > 1) {
             $c = $node->children($ns['vdex']);
             $tempTaxon = (string)$c->termIdentifier;
             $tempLabel = (string)$c->caption->langstring;
-            call_user_func(insertCategory,$source_url,$tempTaxon,$tempLabel);
-
-
+            insertCategory($source_url,$tempTaxon,$tempLabel);
         }
     }
-    print_r($nodes, true);
-
+}
+else
+{
+    echo "Specify all the desired references i.e.\n";
+    echo "php categories.php https://vdex.kennisnet.nl/kennisnetset/2015.01/mbo_opleidingsdomeinen_studierichtingen-knset.xml https://vdex.kennisnet.nl/kennisnetset/2015.01/bve_domeinoverstijgende_vakken-knset.xml
+\n";
 }
 
 // Tabel clearen + OAI-Category table creation
@@ -52,7 +55,7 @@ function createCategoryTable() {
     global $xerte_toolkits_site;
     $prefix = $xerte_toolkits_site->database_table_prefix;
 
-    $q = "CREATE TABLE IF NOT EXISTS oai_categories(
+    $q = "CREATE TABLE IF NOT EXISTS {$prefix}oai_categories(
     category_id INT(11) PRIMARY KEY NOT NULL,
     taxon VARCHAR(64) NOT NULL,
     label VARCHAR(255) NOT NULL,
@@ -65,7 +68,7 @@ function clearCategoryTable() {
     global $xerte_toolkits_site;
     $prefix = $xerte_toolkits_site->database_table_prefix;
 
-    $q = "delete from toolkits_data.syndicationcategories";
+    $q = "delete from {$xerte_toolkits_site->database_table_prefix}syndicationcategories";
 
     db_query($q);
 }
@@ -74,14 +77,14 @@ function insertCategory($source_url, $taxon, $label){
     global $xerte_toolkits_site;
     $prefix = $xerte_toolkits_site->database_table_prefix;
 
-    $q = "INSERT INTO toolkits_data.syndicationcategories(category_name) VALUES (?)";
+    $q = "INSERT INTO {$xerte_toolkits_site->database_table_prefix}syndicationcategories(category_name) VALUES (?)";
     db_query($q,array($label));
 
-    $q2 = "SELECT category_id,category_name FROM toolkits_data.syndicationcategories WHERE category_name like ?";
+    $q2 = "SELECT category_id,category_name FROM {$xerte_toolkits_site->database_table_prefix}syndicationcategories WHERE category_name like ?";
     $result = db_query($q2,array($label));
     $return_id = $result[0]['category_id'];
 
-    $q3 = "INSERT INTO toolkits_data.oai_categories(category_id,taxon,label,source_url) VALUES (?,?,?,?)";
+    $q3 = "INSERT INTO {$xerte_toolkits_site->database_table_prefix}oai_categories(category_id,taxon,label,source_url) VALUES (?,?,?,?)";
     $params = array($return_id,$taxon,$label,$source_url);
     db_query($q3,$params);
 
