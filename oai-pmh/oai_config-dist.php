e541cf791b8069854a143af2ced7ae141cc450bf
Tom Reijnders <reijnders@tor.nl>
2022-04-26 13:44:41 +0200

    Move oai_config.php to oai_config.dist.php

---
+++ oai-pmh/oai_config-dist.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by JetBrains PhpStorm.
+ * User: tom
+ * Date: 19-8-13
+ * Time: 16:16
+ * To change this template use File | Settings | File Templates.
+ */
+
+$config = array(
+    'institute' => "Institute",
+    'adminEmail' => "mail@example.com",
+    'thumbnail' => $xerte_toolkits_site->site_url . 'oai-pmh/xerteobject.png',
+);
