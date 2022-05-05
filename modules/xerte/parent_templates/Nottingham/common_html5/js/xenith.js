e644da05a0c3eedf751578ccd3f41453ebdf2831
Smith <jjs@gcu.ac.uk>
2022-05-05 16:26:42 +0100

    Hopefully this fixes the issues that cropped up with 0563e62

--- modules/xerte/parent_templates/Nottingham/common_html5/js/xenith.js
+++ modules/xerte/parent_templates/Nottingham/common_html5/js/xenith.js
@@ -4370,7 +4370,7 @@ var XENITH = (function ($, parent) { var self = parent.GLOSSARY = {};
 					$(this).trigger("mouseleave");
 
 					var $this = $(this),
-						myText = $this.text().trim(),
+						myText = $this.text().replace(/(\s|&nbsp;)+/g, " ").trim(),
 						myDefinition, i, len;
 
 					for (i=0, len=x_glossary.length; i<len; i++) {
@@ -4463,13 +4463,13 @@ var XENITH = (function ($, parent) { var self = parent.GLOSSARY = {};
 		// check text for glossary words - if found replace with a link
 		if (x_glossary.length > 0 && (exclude == undefined || (exclude == false && list.indexOf("glossary") > -1) || (exclude == true && list.indexOf("glossary") == -1))) {
 			for (var k=0, len=x_glossary.length; k<len; k++) {
-				let term = ignore_space ? x_glossary[k].word.replace(/\s/, '(\\s|&nbsp;)+') : x_glossary[k].word;
-				let regExp = new RegExp('(^|[\\s\(>]|&nbsp;)(' + term + ')([\\s\\.,!?:;\)<]|$|&nbsp;)', multiple_terms ? 'ig' : 'i');
-				tempText = tempText.replace(regExp, '$1{|{'+k+'::$2}|}$3');
+				let term = ignore_space ? x_glossary[k].word.replace(/\s/g, '(?:\\s|&nbsp;)+') : x_glossary[k].word; console.log('\\b(' + term + ')\\b');
+				let regExp = new RegExp('\\b(' + term + ')\\b', multiple_terms ? 'ig' : 'i');
+				tempText = tempText.replace(regExp, '{|{'+k+'::$1}|}'); console.log(tempText);
 			}
 			for (var k=0, len=x_glossary.length; k<len; k++) {
-				let regExp = new RegExp('(^|[\\s\(>]|&nbsp;)(\\{\\|\\{' + k + '::(.*?)\\}\\|\\})([\\s\\.,!?:;\)<]|$|&nbsp;)', multiple_terms ? 'ig' : 'i');
-				tempText = tempText.replace(regExp, '$1<span class="x_glossary" aria-describedby="x_glossaryHover" tabindex="0" role="link">$3</span>$4');
+				let regExp = new RegExp('{\\|{' + k + '::(.*?)}\\|}', multiple_terms ? 'ig' : 'i');
+				tempText = tempText.replace(regExp, '<span class="x_glossary" aria-describedby="x_glossaryHover" tabindex="0" role="link">$1</span>');
 			}
 		}
 		
