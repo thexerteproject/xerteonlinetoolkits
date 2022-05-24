2f297e4d87891c42cf25d1f0e5aeda92133195c8
Tom Reijnders <reijnders@tor.nl>
2022-05-24 11:51:36 +0200

    Cleanup and addition of hints to meta data form

--- oai-pmh/management/rights.php
+++ oai-pmh/management/rights.php
@@ -5,6 +5,7 @@ require_once('../../config.php');
 // php rights.php ./vocabularies/copyrightsandotherrestrictions.xml
 
 if ($argc > 1) {
+    createRightsTable();
     clearRightsTable();
 
     $xmlfile = $argv[1];
@@ -27,40 +28,49 @@ if ($argc > 1) {
         $c = $node->children();
 
         $tempLabel = (string)$c->caption->langstring;
-        insertRights($tempLabel);
+        $tempID = (string)$c->termIdentifier;
+        insertRights($tempID, $tempLabel);
     }
 }
-/*
 
 function createRightsTable() {
     global $xerte_toolkits_site;
     $prefix = $xerte_toolkits_site->database_table_prefix;
 
     $q = "CREATE TABLE IF NOT EXISTS {$prefix}oai_rights(
-    rights_id INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    rights_id INT(11) PRIMARY KEY NOT NULL,
     term_id VARCHAR(63) NOT NULL,
     label VARCHAR(255) NOT NULL)";
 
     db_query($q);
 }
-*/
 
 function clearRightsTable() {
     global $xerte_toolkits_site;
     $prefix = $xerte_toolkits_site->database_table_prefix;
 
-    $q = "delete from {$xerte_toolkits_site->database_table_prefix}syndicationlicenses";
+    $q = "delete from {$prefix}syndicationlicenses";
+    db_query($q);
 
+    $q = "delete from {$prefix}oai_rights";
     db_query($q);
 }
 
-function insertRights($label){
+function insertRights($term_id, $label){
     global $xerte_toolkits_site;
     $prefix = $xerte_toolkits_site->database_table_prefix;
 
-    $q = "INSERT INTO {$xerte_toolkits_site->database_table_prefix}syndicationlicenses(license_name) VALUES (?)";
+    $q = "INSERT INTO {$prefix}syndicationlicenses(license_name) VALUES (?)";
     $params = array($label);
     db_query($q,$params);
+
+    $q2 = "SELECT license_id FROM {$prefix}syndicationlicenses WHERE license_name like ?";
+    $result = db_query($q2,array($label));
+    $return_id = $result[0]['license_id'];
+
+    $q3 = "INSERT INTO {$prefix}oai_rights(rights_id,term_id,label) VALUES (?,?,?)";
+    $params = array($return_id,$term_id,$label);
+    $res = db_query($q3,$params);
 
 }
 
