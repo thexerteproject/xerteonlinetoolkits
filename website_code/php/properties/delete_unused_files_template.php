94ccb2ac403e8734f5cc45cb9e914651df82c421
Timo Boer <omitreob@hotmail.com>
2022-11-10 10:21:32 +0100

    removed obsolete code

--- website_code/php/properties/delete_unused_files_template.php
+++ website_code/php/properties/delete_unused_files_template.php
@@ -47,10 +47,3 @@ foreach ($data as $d) {
     }
 
 }
-
-
-if(unlink(urldecode($_POST['file']))){
-    receive_message($_SESSION['toolkits_logon_username'], "FILE", "SUCCESS", "The file " . $_POST['file'] . "has been deleted", "User " . $_SESSION['toolkits_logon_username'] . " has deleted " . $_POST['file']);
-}else{
-    receive_message($_SESSION['toolkits_logon_username'], "FILE", "MAJOR", "The file " . $_POST['file'] . "hasn't been deleted", "User " . $_SESSION['toolkits_logon_username'] . " was not deleted " . $_POST['file']);
-}
