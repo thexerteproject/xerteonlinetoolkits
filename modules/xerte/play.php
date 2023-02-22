2d7ffeceb0f3c4c21ce10b4a7c3619adb7972b83
Tom Reijnders <reijnders@tor.nl>
2023-02-22 16:26:30 +0100

    Partly fix for #1194 - critical error in develop after latest changes
     - Check if plugins folder exists
    
    The other part of this issue is fixed by RonM

--- modules/xerte/play.php
+++ modules/xerte/play.php
@@ -122,23 +122,24 @@ function show_template_page($row, $datafile="", $xapi_enabled = false)
     }
 
     // Get plugins
-    $pluginfiles = scandir($template_path . "plugins/");
     $plugins = array();
-    foreach($pluginfiles as $pluginfile) {
-        // get base name of plugin
-        $plugininfo = pathinfo($pluginfile);
-        if ($plugininfo['basename'] == '.' || $plugininfo['basename'] == '..') {
-            continue;
-        }
-        if (!isset($plugins[$plugininfo['filename']]))
-        {
-            $plugins[$plugininfo['filename']] = new stdClass();
-        }
-        if ($plugininfo['extension'] == 'js') {
-            $plugins[$plugininfo['filename']]->script = file_get_contents($template_path . "plugins/" . $pluginfile);
-        }
-        if ($plugininfo['extension'] == 'css') {
-            $plugins[$plugininfo['filename']]->css = file_get_contents($template_path . "plugins/" . $pluginfile);
+    if (file_exists($template_path . "plugins")) {
+        $pluginfiles = scandir($template_path . "plugins/");
+        foreach ($pluginfiles as $pluginfile) {
+            // get base name of plugin
+            $plugininfo = pathinfo($pluginfile);
+            if ($plugininfo['basename'] == '.' || $plugininfo['basename'] == '..') {
+                continue;
+            }
+            if (!isset($plugins[$plugininfo['filename']])) {
+                $plugins[$plugininfo['filename']] = new stdClass();
+            }
+            if ($plugininfo['extension'] == 'js') {
+                $plugins[$plugininfo['filename']]->script = file_get_contents($template_path . "plugins/" . $pluginfile);
+            }
+            if ($plugininfo['extension'] == 'css') {
+                $plugins[$plugininfo['filename']]->css = file_get_contents($template_path . "plugins/" . $pluginfile);
+            }
         }
     }
     $rlo_object_file = "rloObject.htm";
