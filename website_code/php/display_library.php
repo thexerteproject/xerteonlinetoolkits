thexerteproject/xerteonlinetoolkits

commit f571dfc6bd8d8e9027f5d0c65d747e200ba7fec2

Fix incomplete group_by for default mode of mysql 8

website_code/php/display_library.php
@@ -783,7 +783,7 @@ function get_workspace_folders($folder_id, $tree_id, $sort_type, $copy_only=fals
 select fd.folder_id, fd.folder_name, fr.folder_parent, fr.id, fr.role, count(fr.folder_id) as nrshared  from {$prefix}folderdetails fd, {$prefix}folderrights fr  where fr.folder_id = fd.folder_id  and fd.folder_parent != 0 and fr.folder_id in 
                     (
                         select fd.folder_id from {$prefix}folderdetails fd, {$prefix}folderrights fr where fr.folder_id = fd.folder_id AND fr.login_id=? and fd.folder_parent != 0
-                    )  GROUP BY  fr.folder_id
+                    )  GROUP BY  fr.folder_id, fr.folder_parent, fr.id, fr.role
 ) as cfr  where fr.folder_id = fd.folder_id and fr.folder_id = cfr.folder_id AND fr.login_id=? and fd.folder_parent != 0 
 ";
         $params = array($_SESSION['toolkits_logon_id'], $_SESSION['toolkits_logon_id']);

website_code/php/properties/properties_library.php
@@ -849,7 +849,7 @@ function sharing_info($template_id)
 
         }
     }
-    $sql .= ") group by ld.login_id";
+    $sql .= ") group by ld.login_id, fr.role";
     $query_shared_folder_users = db_query($sql, $params);
 
     $roles = array("creator"=>4, "co-author"=>3, "editor"=>2, "read-only"=>1);
@@ -1133,12 +1133,20 @@ function oai_shared($template_id){
 
     $sql = "select status from {$xerte_toolkits_site->database_table_prefix}oai_publish where template_id=? ORDER BY audith_id DESC LIMIT 1";
     $params = array($_POST['template_id']);
-    $last_oaiTable_status = db_query_one($sql, $params)["status"];
+    $status = db_query_one($sql, $params);
     $info = PROJECT_INFO_OAI . ": ";
-    if (is_null($last_oaiTable_status) || $last_oaiTable_status != "published") {
+    if ($status == null)
+    {
         $info .= PROJECT_INFO_NOTSHARED . "<br/>";
-    } else {
-        $info .= PROJECT_INFO_SHARED . "<br/>";
+    }
+    else {
+        $last_oaiTable_status = $status["status"];
+
+        if (is_null($last_oaiTable_status) || $last_oaiTable_status != "published") {
+            $info .= PROJECT_INFO_NOTSHARED . "<br/>";
+        } else {
+            $info .= PROJECT_INFO_SHARED . "<br/>";
+        }
     }
     return $info;
 }

properties.php
@@ -290,23 +290,7 @@ if (in_array("give", $template_supports)) {
                             -->
                             <?PHP
                         }
-                        if (nr_user_groups() > 0)
-                        {
                         ?>
-<!--
-                        <div class="tab_spacer" style="height:<?PHP echo (((5 - count($template_supports)) * 53)) + 18; ?>px;">
-                        </div>
-                        <div id="tab12" class="tab"  style="width:146px; height:38px;">
-                            <p onclick="javascript:tab_highlight('12');
-                                                                                                        group_sharing_status_template()">
-                                <i class="fas fa-users xerte-icon"></i><?PHP echo PROPERTIES_TAB_GROUP_SHARED; ?>
-                            </p>
-                        </div>
-                        -->
-                        <?php
-                        }
-                        ?>
-
                         <div class="tab_spacer">
                     </div>
                 </div>									

commit 8ddfb9f03f6b8fcc3a4760edc769fc3696c71b52

Fix foldersharing and group by mode errors
 - Folder sharing was broken by commit f571dfc6bd8d8e9027f5d0c65d747e200ba7fec2
 - Retested all group by clauses
 - Fixed typo in folder status

languages/en-GB/website_code/php/properties/sharing_status_template.inc
@@ -16,6 +16,8 @@
 
 	define("SHARING_CURRENT","This project is currently shared with : ");
 
+    define("SHARING_CURRENT_FOLDER","This folder is currently shared with : ");
+
     define("SHARING_CREATOR", "Creator");
 
     define("SHARING_COAUTHOR", "Co-author");

website_code/php/display_library.php
@@ -153,7 +153,7 @@ function list_folder_contents_event_free($folder_id, $path = '', $item = false, 
  * @author Patrick Lockley
  */
 // TODO depracate!!
-function list_folders_in_this_folder($folder_id, $sort_type){
+function list_folders_in_this_folder_depracated($folder_id, $sort_type){
 
   /*
   * use the global level for folder indenting
@@ -233,7 +233,7 @@ function list_folders_in_this_folder($folder_id, $sort_type){
  * @author Patrick Lockley
  */
 // TODO depracate!!
-function list_files_in_this_folder($folder_id, $sort_type) {
+function list_files_in_this_folder_depracated($folder_id, $sort_type) {
 
   global $level, $xerte_toolkits_site;
 
@@ -276,7 +276,7 @@ function list_files_in_this_folder($folder_id, $sort_type) {
  * @author Patrick Lockley
  */
 // TODO depracate!!
-function list_folder_contents($folder_id, $sort_type) {
+function list_folder_contents_depracated($folder_id, $sort_type) {
 
   global $level;
 
@@ -296,7 +296,7 @@ function list_folder_contents($folder_id, $sort_type) {
  * @author Patrick Lockley
  */
 // TODO depracate!!
-function list_users_projects($sort_type) {
+function list_users_projects_depracated($sort_type) {
 
   /*
   * Called by index.php to start off the process
@@ -502,7 +502,7 @@ function get_files_in_this_folder($folder_id, $tree_id, $sort_type, $copy_only, 
     }
 
     if ($type != "group_top") {
-        $query .= " group by td.template_id, td.template_name, td.date_created, otd.template_name,td.access_to_whom, td.tsugi_published, otd.parent_template, otd.template_framework, tr.role, tr.folder,fd3.folder_name ";
+        $query .= " group by td.template_id, td.creator_id, td.template_name, td.date_created, otd.template_name,td.access_to_whom, td.tsugi_published, otd.parent_template, otd.template_framework, tr.role, tr.folder,fd3.folder_name ";
     }
 
     $top = false;
@@ -779,12 +779,12 @@ function get_workspace_folders($folder_id, $tree_id, $sort_type, $copy_only=fals
             . " where fd.folder_id = fgr.folder_id AND fgr.group_id = ?";
         $params = array($folder_id);
     }else{
-        $query = "select fd.folder_id, fd.folder_name, fd.folder_parent as real_parent, fr.folder_parent, fr.role, cfr.nrshared as nrshared from {$prefix}folderdetails fd, {$prefix}folderrights fr cross join (
-select fd.folder_id, fd.folder_name, fr.folder_parent, fr.id, fr.role, count(fr.folder_id) as nrshared  from {$prefix}folderdetails fd, {$prefix}folderrights fr  where fr.folder_id = fd.folder_id  and fd.folder_parent != 0 and fr.folder_id in 
+        $query = "select fd.folder_id, fd.folder_name, fr.folder_parent, fr.role, cfr.nrshared as nrshared from {$prefix}folderdetails fd, {$prefix}folderrights fr cross join (
+select fr.folder_id, count(fr.folder_id) as nrshared  from {$prefix}folderdetails fd, {$prefix}folderrights fr  where fr.folder_id = fd.folder_id  and fr.folder_parent != 0 and fr.folder_id in 
                     (
-                        select fd.folder_id from {$prefix}folderdetails fd, {$prefix}folderrights fr where fr.folder_id = fd.folder_id AND fr.login_id=? and fd.folder_parent != 0
-                    )  GROUP BY  fr.folder_id, fr.folder_parent, fr.id, fr.role
-) as cfr  where fr.folder_id = fd.folder_id and fr.folder_id = cfr.folder_id AND fr.login_id=? and fd.folder_parent != 0 
+                        select fd.folder_id from {$prefix}folderdetails fd, {$prefix}folderrights fr where fr.folder_id = fd.folder_id AND fr.login_id=? and fr.folder_parent != 0
+                    )  GROUP BY  fr.folder_id
+) as cfr  where fr.folder_id = fd.folder_id and fr.folder_id = cfr.folder_id AND fr.login_id=? and fr.folder_parent != 0 
 ";
         $params = array($_SESSION['toolkits_logon_id'], $_SESSION['toolkits_logon_id']);
     }

website_code/php/properties/properties_library.php
@@ -849,7 +849,8 @@ function sharing_info($template_id)
 
         }
     }
-    $sql .= ") group by ld.login_id, fr.role";
+    $sql .= ") ";
+    //$sql .= ") group by ld.login_id, fr.role";
     $query_shared_folder_users = db_query($sql, $params);
 
     $roles = array("creator"=>4, "co-author"=>3, "editor"=>2, "read-only"=>1);
@@ -979,7 +980,7 @@ function folder_sharing_info($folder_id)
         return $info;
     }
 
-    $info .=  SHARING_CURRENT . "<br>";
+    $info .=  SHARING_CURRENT_FOLDER . "<br>";
     foreach($query_sharing_rows as $row) {
         $info .=  "<li><span>" . $row['firstname'] . " " . $row['surname'] ." (" .$row['username'] . ")  -  (";
         switch($row['role'])

website_code/scripts/screen_display.js
@@ -276,7 +276,7 @@ function screen_refresh_no_ajax(){
 	 * @author Patrick Lockley
 	 */
 //TODO : depracate
-function screen_refresh(){
+function screen_refresh_depracated(){
     $.ajax({
         type: "POST",
         url: "website_code/php/templates/your_templates.php",