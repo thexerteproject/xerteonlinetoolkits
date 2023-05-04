2e67e3b120988e680b5f555c3e5d01500b2f15a6
Tom Reijnders <reijnders@tor.nl>
2023-05-04 23:12:44 +0200

    Fixed #1213 - previous fix did not work if theme was not set at all
    This happens with very old projects (in this case 2015)

--- modules/xerte/parent_templates/Nottingham/common_html5/js/xenith.js
+++ modules/xerte/parent_templates/Nottingham/common_html5/js/xenith.js
@@ -757,6 +757,7 @@ function x_getThemeInfo(thisTheme, themeChg) {
 	// some older themes use images for interface buttons (not FontAwesome icons) - it's only these themes that can fall back to use the defaultFA (all others should have FA icons set in theme)
 	// these themes should have imgbtns: true in the theme info file
 	if (thisTheme == undefined || thisTheme == "default") {
+		x_params.theme = "default";
 		x_setUpThemeBtns({ imgbtns: 'true' }, themeChg);
 		
 	} else {
