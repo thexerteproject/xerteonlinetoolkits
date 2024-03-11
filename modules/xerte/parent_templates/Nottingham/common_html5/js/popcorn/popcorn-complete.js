3eb5293a94c10c23a161c18e8ff239aa7de3a287
mussaf <38488831+mussaf@users.noreply.github.com>
2024-03-11 14:30:29 +0100

    init yuja

--- modules/xerte/parent_templates/Nottingham/common_html5/js/popcorn/popcorn-complete.js
+++ modules/xerte/parent_templates/Nottingham/common_html5/js/popcorn/popcorn-complete.js
@@ -3230,7 +3230,7 @@
         // We leave HTMLVideoElement and HTMLAudioElement wrappers out
         // of the mix, since we'll default to HTML5 video if nothing
         // else works.  Waiting on #1254 before we add YouTube to this.
-        wrappers = "HTMLYouTubeVideoElement HTMLVimeoVideoElement HTMLMediasiteVideoElement HTMLPeerTubeVideoElement HTMLSoundCloudAudioElement HTMLNullVideoElement".split(" ");
+        wrappers = "HTMLYouTubeVideoElement HTMLVimeoVideoElement HTMLMediasiteVideoElement HTMLPeerTubeVideoElement HTMLYujaVideoElement HTMLSoundCloudAudioElement HTMLNullVideoElement".split(" ");
 
     if ( !node ) {
       Popcorn.error( "Specified target `" + target + "` was not found." );
@@ -4918,6 +4918,287 @@
       ? "probably" 
       : "";  
     };
+}( Popcorn, window, document ));
+
+(function( Popcorn, window, document )
+{
+  var CURRENT_TIME_MONITOR_MS = 16;
+
+  function HTMLYujaVideoElement(id)
+  {
+    debugger
+    var self = new Popcorn._MediaElementProto(),
+        parent = typeof id === "string" ? Popcorn.dom.find( id ) : id,
+        elem = document.createElement( "iframe" ),
+        impl = {
+          src: "",
+          networkState: self.NETWORK_EMPTY,
+          readyState: self.HAVE_NOTHING,
+          seeking: false,
+          autoplay: "",
+          preload: "",
+          controls: false,
+          loop: false,
+          poster: "",
+          volume: 1,
+          muted: 0,
+          currentTime: 0,
+          duration: NaN,
+          ended: false,
+          paused: true,
+          error: null
+        },
+        playerReady = false,
+        player,
+        playerPaused = true,
+        playerReadyCallbacks = [],
+        timeUpdateInterval,
+        currentTimeInterval;
+
+    // Namespace all events we'll produce
+    self._eventNamespace = Popcorn.guid( "HTMLPeerTubeVideoElement::" );
+
+    self.parentNode = parent;
+
+    // Mark type as Mediasite
+    self._util.type = "Peertube";
+
+    elem.id = "peertubeIframe";
+    elem.addClass = " .iframe ";
+
+    function addPlayerReadyCallback( callback ) {
+      playerReadyCallbacks.push( callback );
+    }
+
+    function changeSrc( aSrc )
+    {
+      if (!self._canPlaySrc(aSrc)) {
+        impl.error = {
+          name: "MediaError",
+          message: "Media Source Not Supported",
+          code: MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED
+        };
+        self.dispatchEvent("error");
+        return;
+      }
+      impl.src = aSrc;
+      parent.appendChild(elem);
+      self.dispatchEvent("loadstart");
+      self.dispatchEvent("progress");
+      elem.width="100%";
+      elem.height="100%";
+      elem.src=aSrc + "?api=1";
+      elem.frameborder="0";
+      elem.allowfullscreen="";
+      elem.sandbox="allow-same-origin allow-scripts allow-popups";
+
+      if (!xot_offline && typeof PeerTubePlayer !== undefined) {
+        $.getScript("https://unpkg.com/@peertube/embed-api/build/player.min.js")
+            .done(function () {
+                  onReady();
+                }
+            ).fail(function () {
+        });
+      }
+      else {
+        onReady();
+      }
+    }
+
+    //Called when the player script has loaded
+    function onReady()
+    {
+      const PeerTubePlayer = window['PeerTubePlayer']
+      player = new PeerTubePlayer(elem);
+      player.addEventListener("playbackStatusUpdate", function(videoState) {monitorCurrentTime(videoState)}, false);
+
+      impl.readyState = self.HAVE_FUTURE_DATA;
+      self.dispatchEvent( "canplay" );
+
+      impl.readyState = self.HAVE_ENOUGH_DATA;
+      self.dispatchEvent( "canplaythrough" );
+
+      playerReady = true;
+    }
+
+
+    function monitorCurrentTime(videoState) {
+      var playerTime = videoState.position;
+      if (impl.duration != videoState.duration) {
+        impl.duration = videoState.duration;
+        self.dispatchEvent( "durationchange" );
+        if (impl.duration > 0) {
+          self.dispatchEvent( "loadedmetadata" );
+          self.dispatchEvent( "loadeddata" );
+        }
+      }
+
+      if (impl.currentTime != videoState.position) {
+        self.dispatchEvent("timeupdate");
+      }
+
+      // Playbackstates are: playing, notstarted, paused and ended. Only playing is relevant for us.
+      var isPaused = !(videoState.playbackState == 'playing');
+      if (isPaused != impl.playerPaused) {
+        if (isPaused) {
+          onPause();
+        }
+        else {
+          onPlay();
+        }
+        impl.playerPaused = isPaused;
+      }
+
+      if ( !impl.seeking ) {
+        if ( Math.abs( impl.currentTime - playerTime ) > CURRENT_TIME_MONITOR_MS ) {
+          onSeeking();
+          onSeeked();
+        }
+        impl.currentTime = playerTime;
+      } else if ( Math.abs( playerTime - impl.currentTime ) < 1 ) {
+        onSeeked();
+      }
+    }
+
+    function changeCurrentTime( aTime ) {
+      if (aTime === impl.currentTime) {
+        return;
+      }
+      impl.currentTime = aTime;
+      onSeeking();
+      player.seek(aTime);
+    }
+
+    function onPlay() {
+      if( impl.ended ) {
+        changeCurrentTime( 0 );
+        impl.ended = false;
+      }
+      self.dispatchEvent( "durationchange" );
+      impl.paused = false;
+      if( playerPaused ) {
+        playerPaused = false;
+        self.dispatchEvent( "play" );
+        self.dispatchEvent( "playing" );
+      }
+    }
+
+    self.play = function() {
+      impl.paused = false;
+      if( !playerReady ) {
+        addPlayerReadyCallback( function() { self.play(); } );
+        return;
+      }
+
+      player.play();
+    };
+
+    function onPause () {
+      impl.paused = true;
+      if ( !playerPaused ) {
+        playerPaused = true;
+        clearInterval( timeUpdateInterval );
+        self.dispatchEvent( "pause" );
+      }
+    }
+
+    self.pause = function() {
+      impl.paused = true;
+      if( !playerReady ) {
+        addPlayerReadyCallback( function() { self.pause(); } );
+      }
+      player.pause();
+    };
+
+    function onSeeking() {
+      impl.seeking = true;
+      self.dispatchEvent( "seeking" );
+    }
+
+    function onSeeked() {
+      impl.ended = false;
+      impl.seeking = false;
+
+      self.dispatchEvent( "timeupdate" );
+      self.dispatchEvent( "seeked" );
+      self.dispatchEvent( "canplay" );
+      self.dispatchEvent( "canplaythrough" );
+    }
+
+    function onEnded() {
+      impl.ended = true;
+      onPause();
+      self.dispatchEvent("timeupdate");
+      self.dispatchEvent("ended");
+    }
+
+    Object.defineProperties( self, {
+
+      src: {
+        get: function () {
+          return impl.src;
+        },
+        set: function (aSrc) {
+          if (aSrc && aSrc !== impl.src) {
+            changeSrc(aSrc);
+          }
+        }
+      },
+
+      seeking: {
+        get: function() {
+          return impl.seeking;
+        }
+      },
+
+      loop: {
+        get: function() {
+          return impl.loop;
+        },
+        set: function( aValue ) {
+          impl.loop = self._util.isAttributeSet( aValue );
+        }
+      },
+
+      paused: {
+        get: function() {
+          return impl.paused;
+        }
+      },
+
+      duration: {
+        get: function() {
+          return impl.duration;
+        }
+      },
+
+      currentTime: {
+        get: function() {
+          return impl.currentTime;
+        },
+        set: function( aValue ) {
+          changeCurrentTime( aValue );
+        }
+      }
+    });
+
+    self._canPlaySrc = Popcorn.HTMLYujaVideoElement._canPlaySrc;
+
+    return self;
+  }
+
+
+  Popcorn.HTMLYujaVideoElement = function( id ) {
+    return new HTMLYujaVideoElement( id );
+  };
+
+  // Helper for identifying URLs we know how to play.
+  Popcorn.HTMLYujaVideoElement._canPlaySrc = function( url ) {
+    debugger
+    return (/\/videos\/embed\/[a-z0-9]{8}[-][a-z0-9]{4}[-][a-z0-9]{4}[-][a-z0-9]{4}[-][a-z0-9]{12}$/).test( url ) || isPeertubeVideo(url)
+        ? "probably"
+        : "";
+  };
 }( Popcorn, window, document ));
 
 
