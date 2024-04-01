a4f47337bebdf73675b3c1fa5a7a6039caa5738b
Tom Reijnders <reijnders@tor.nl>
2024-04-01 22:25:15 +0200

    Set proper lti context id (needs latest tsugi as of 03/2024)

--- lti_launch.php
+++ lti_launch.php
@@ -71,6 +71,7 @@ if(is_numeric($id) || $id == null)
         $nrps = $LAUNCH->context->loadNamesAndRoles(false, $msg);
         //DONE: get all emails of current users add to array
         // https://gitlab.tor.nl/xerte-dashboard/dashboard/-/blob/master/index.php line 80
+	_debug('LTI 1.3 names and roles result: ' . print_r($nrps, true));
         $xerte_toolkits_site->lti_users = array();
         foreach ($nrps->members as $i => $member){
             if ($member->status == 'Active' && in_array('Learner', $member->roles))
@@ -135,18 +136,33 @@ if(is_numeric($id) || $id == null)
     {
         $xerte_toolkits_site->module = $module;
     }
+    if (isset($LAUNCH->context->context_id))
+    {
+        $lticontextid = $LAUNCH->context->context_id;
+	_debug('Context id set from context->context_id');
+    }
+    else
+    {
     $lticontextid = $LAUNCH->ltiParameter('context_id');
+    if ($lticontextid !== false)
+    {
+	_debug('Context id set from parameter');
+    }
     if ($lticontextid === false)
     {
         $lticontextid = $LAUNCH->ltiCustomGet('context_id');
+	_debug('Context id set from custom parameter');
     }
     if ($lticontextid===false && isset($_REQUEST['context_id']))
     {
         $lticontextid = $_REQUEST['context_id'];
+	_debug('Context id set from request');
     }
     if ($lticontextid === false)
     {
         $lticontextid = $LAUNCH->context->id;
+	_debug('Context id set from context->id');
+    }
     }
     if ($lticontextid !== false)
     {
