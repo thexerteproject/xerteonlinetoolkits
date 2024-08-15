c3c78cfcfa6a18f7ffb7c508685d3be963935774
Troy van Sleeuwen <troy.vansleeuwen@edu-kw1c.nl>
2024-08-15 11:01:25 +0200

    fixed interactive video score and controls error, change the play link in project properties to pwplay link if enabled, remove peer review password from pwplay

--- website_code/php/properties/properties_library.php
+++ website_code/php/properties/properties_library.php
@@ -133,9 +133,15 @@ function properties_display($xerte_toolkits_site, $template_id, $change, $msgtyp
 
         echo "<p>" . PROPERTIES_LIBRARY_PROJECT_LINK;
 
+        $play_page = "play";
+
+        if(substr(template_access_settings($template_id), 0, 12)=="PasswordPlay"){
+            $play_page = "passwordplay";
+        }
+
         echo "<br/><a target=\"new\" href='" . $xerte_toolkits_site->site_url .
-                url_return("play", $template_id) . "'>" .
-                $xerte_toolkits_site->site_url . url_return("play", $template_id) . PROPERTIES_LIBRARY_PROJECT_LINKS . "</a></p>";
+                url_return($play_page, $template_id) . "'>" .
+                $xerte_toolkits_site->site_url . url_return($play_page, $template_id) . PROPERTIES_LIBRARY_PROJECT_LINKS . "</a></p>";
 
 		$template = explode("_", get_template_type($template_id));
 
