4e839cbdb5f8c5c779555180504f5e75b010f55c
Tom Reijnders <reijnders@tor.nl>
2024-12-15 12:57:47 +0100

    Require both projectadmin and system rights to be able to do bulk transfer

--- website_code/php/management/do_transfer_user_templates.php
+++ website_code/php/management/do_transfer_user_templates.php
@@ -248,7 +248,7 @@ function createGetFolderId($folder_structure, $newuserid, $old_folder_id, $trans
 }
 
 
-if(is_user_permitted("projectadmin"))
+if(is_user_permitted("projectadmin", "system"))
 {
     if (isset($_REQUEST['olduserid']) && isset($_REQUEST['newuserid']) && isset($_REQUEST['transfer_private']) && isset($_REQUEST['transfer_shared_folders']) && isset($_REQUEST['delete_user'])) {
 
