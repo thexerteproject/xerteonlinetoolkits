e0338c21415f00e1171a3186923efe34e3dc5796
Tom Reijnders <reijnders@tor.nl>
2025-01-17 10:23:28 +0100

    Fix potential issue with PHP8.3

--- website_code/php/properties/properties_library.php
+++ website_code/php/properties/properties_library.php
@@ -1072,7 +1072,6 @@ function sharing_info($template_id)
         if (!$found)
         {
             // Add $row to $query_shared_folder_users
-            $index = count($query_shared_folder_users);
             $query_shared_folder_users[] = $row;
             if ($row['role'] == "creator") {
                 // Change the role of the user to co-author
