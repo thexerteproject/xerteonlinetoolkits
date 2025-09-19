6e0a2dd5008b5b1c9de77fca68f7173609bc9893
FayCross <fay.cross@nottingham.ac.uk>
2025-09-19 16:46:14 +0100

    #1475 Improve responsiveness of the column page & add new optional properties to change the page layout

--- modules/xerte/parent_templates/Nottingham/common_html5/js/xenith.js
+++ modules/xerte/parent_templates/Nottingham/common_html5/js/xenith.js
@@ -4294,12 +4294,12 @@ function x_scaleImg(img, maxW, maxH, scale, firstScale, setH, enlarge) {
             imgH = $img.data("origSize")[1];
         }
 
-        if (enlarge === false) {
+        if (enlarge !== false) {
             maxW = Math.min(maxW, imgW);
             maxH = Math.min(maxH, imgH);
         }
 
-        if (imgW > maxW || imgH > maxH || firstScale != true || enlarge !== false) {
+        if (imgW > maxW || imgH > maxH || firstScale != true || enlarge === true) {
             var scaleW = maxW / imgW,
                 scaleH = maxH / imgH,
                 scaleFactor = Math.min(scaleW, scaleH);
