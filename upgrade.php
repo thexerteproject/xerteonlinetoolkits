796c6fe60cdf0bf612986e58a920e0ad0e79e37d
Tom Reijnders <tom.reijnders@dlearning.nl>
2026-01-08 16:30:02 +0100

    Fix upgrade.php
     - Change order of upgrade routines (merge caused wrong sequence of updates)
     - Fix insertion of records in management_helper table

--- upgrade.php
+++ upgrade.php
@@ -1625,16 +1625,6 @@ function upgrade_50()
 
 function upgrade_51()
 {
-    $roleTable = table_by_key("role");
-
-    $ok = db_query("insert into $roleTable(`roleid`, `name`) values (8, 'aiuser')");
-    $message = "Creating extra role aiuser - ok ? " . ($ok ? 'true' : 'false') . "<br>";
-
-    return $message;
-}
-
-function upgrade_52()
-{
     // Add disabled flag to logindetails
     if (! _db_field_exists('logindetails', 'disabled')) {
         $error1 = _db_add_field('logindetails', 'disabled', 'tinyint(1)', '0', 'surname');
@@ -1647,7 +1637,7 @@ function upgrade_52()
     }
 }
 
-function upgrade_53()
+function upgrade_52()
 {
     // Add the following extensions to the blacklisted extensions:
     //php1,php2,php3,php4,php5,php6,php7,php8,phar,phtml,inc,py,bat,cmd,ps,htaccess
@@ -1669,6 +1659,17 @@ function upgrade_53()
         }
     }
 }
+
+function upgrade_53()
+{
+    $roleTable = table_by_key("role");
+
+    $ok = db_query("insert into $roleTable(`roleid`, `name`) values (8, 'aiuser')");
+    $message = "Creating extra role aiuser - ok ? " . ($ok ? 'true' : 'false') . "<br>";
+
+    return $message;
+}
+
 function upgrade_54()
 {
     $message = "";
@@ -1692,20 +1693,20 @@ function upgrade_54()
 
         if ($ok) {
             $ok = db_query("INSERT INTO $table VALUES 
-                                    (1, 'openai', 'GPT (Openai)', 'ai', 1, 0, '{}'),
-                                    (2, 'anthropic', 'Claude (Anthropic)', 'ai', 1, 0, '{}'),
-                                    (3, 'mistral', 'Mistral AI', 'ai', 1, 0, '{}'),
-                                    (4, 'pexels', 'Pexels', 'image', 1, 0, '{}'),
-                                    (5, 'pixabay', 'Pixabay', 'image', 1, 0, '{}'),
-                                    (6, 'unsplash', 'Unsplash', 'image', 1, 0, '{}'),
-                                    (7, 'wikimedia', 'Wikimedia Foundation', 'image', 0, 0, '{}'),
-                                    (8, 'dalle2', 'DallE2 (Generative)', 'imagegen', 1, 0, '{}'),
-                                    (9, 'dalle3', 'DallE3 (Generative)', 'imagegen', 1, 0, '{}'),
-                                    (10, 'gpt1', 'GPT Image 1', 'imagegen', 1, 0, '{}'),
-                                    (11, 'gladia', 'Gladia (Transcription)', 'transcription', 1, 0, '{}'),
-                                    (12, 'openai', 'Open AI (Transcription)', 'transcription', 1, 0, '{}'),
-                                    (13, 'mistralenc', 'Mistral (Encoding)', 'encoding', 1, 0, '{}'),
-                                    (14, 'openaienc', 'OpenAI (Encoding)', 'encoding', 1, 0, '{}')
+                                    (1, 'openai', 'GPT (Openai)', 'ai', 1, 0, '{}', ''),
+                                    (2, 'anthropic', 'Claude (Anthropic)', 'ai', 1, 0, '{}', ''),
+                                    (3, 'mistral', 'Mistral AI', 'ai', 1, 0, '{}', ''),
+                                    (4, 'pexels', 'Pexels', 'image', 1, 0, '{}', ''),
+                                    (5, 'pixabay', 'Pixabay', 'image', 1, 0, '{}', ''),
+                                    (6, 'unsplash', 'Unsplash', 'image', 1, 0, '{}', ''),
+                                    (7, 'wikimedia', 'Wikimedia Foundation', 'image', 0, 0, '{}', ''),
+                                    (8, 'dalle2', 'DallE2 (Generative)', 'imagegen', 1, 0, '{}', ''),
+                                    (9, 'dalle3', 'DallE3 (Generative)', 'imagegen', 1, 0, '{}', ''),
+                                    (10, 'gpt1', 'GPT Image 1', 'imagegen', 1, 0, '{}', ''),
+                                    (11, 'gladia', 'Gladia (Transcription)', 'transcription', 1, 0, '{}', ''),
+                                    (12, 'openai', 'Open AI (Transcription)', 'transcription', 1, 0, '{}', ''),
+                                    (13, 'mistralenc', 'Mistral (Encoding)', 'encoding', 1, 0, '{}', ''),
+                                    (14, 'openaienc', 'OpenAI (Encoding)', 'encoding', 1, 0, '{}', '')
                                     ");
             $message .= "Populating management helper table - ok ? " . ($ok ? 'true' : 'false') . "<br>";
         }
