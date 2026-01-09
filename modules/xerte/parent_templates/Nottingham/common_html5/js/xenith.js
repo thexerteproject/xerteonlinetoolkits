thexerteproject/xerteonlinetoolkits

commit c64e2bf16ab5b3bd687939cc267d495e36722c8d

#1476 New option to centre panel/column content vertically

modules/xerte/parent_templates/Nottingham/common_html5/js/xenith.js
@@ -2556,9 +2556,9 @@ function x_checkPages(type, id, pageArray) {
 function x_setUpPagePosition(project) {
 	// remove classes that might have been used to turn on/off page position settings at page level
 	if (project !== true) {
-		$x_mainHolder.removeClass("x_noVerticalCentre");
+		$x_mainHolder.removeClass("x_noVerticalCentre x_noVerticalCentrePanel");
 		$x_pageDiv.removeClass("x_noMaxW");
-		$x_pageHolder.removeClass("x_verticalCentre");
+		$x_pageHolder.removeClass("x_verticalCentre x_verticalCentrePanel");
 	}
 
 	// set a maximum width and align content horizontally on the screen (centre by default)
@@ -2607,27 +2607,44 @@ function x_setUpPagePosition(project) {
 	}
 
 	// centre page content vertically on the screen
-	if ((project && x_params.verticalAlign === "true") || project !== true) {
-		if (project && x_params.verticalAlign === "true") {
-			// turn on for the whole project
-			$x_mainHolder.addClass("x_verticalCentre");
-
-		} else if (project !== true) {
-			// these pages don't work with vertical centre yet - some of them will break & others are never able to be centred as the content is always at least full height
-			const nonWorkingPages = ['adaptiveContent','audioSlideshow','chart','embedDiv','hotspotImage','imageCompare','imageSequence','interactiveVideo','media360','mediaLesson','memory','perspectives','rss','rssdownload','SictTimeline','tabNavExtra','textHighlight','thumbnailViewer','title','videoSynch'];
-			if (x_currentPageXML.getAttribute("verticalAlign") != undefined && (nonWorkingPages == undefined || nonWorkingPages.indexOf(x_pageInfo[x_currentPage].type) < 0)) {
-				if (x_currentPageXML.getAttribute("verticalAlign") === "true") {
-					// turn on for this page
-					$x_pageHolder.addClass("x_verticalCentre");
-				} else if (x_currentPageXML.getAttribute("verticalAlign") === "false") {
-					// turn off for this page
-					$x_mainHolder.addClass("x_noVerticalCentre");
-				}
-			} else if (nonWorkingPages != undefined && nonWorkingPages.indexOf(x_pageInfo[x_currentPage].type) >= 0) {
-				// always turn off for this type of page as it won't work properly
-				$x_mainHolder.addClass("x_noVerticalCentre");
-			}
+	// related option individually centres columns vertically
+	if (project && x_params.verticalAlign === "true") {
+		// turn on for the whole project
+		$x_mainHolder.addClass("x_verticalCentre");
+		if (x_params.verticalAlignPanel === "true") {
+			$x_mainHolder.addClass("x_verticalCentrePanel");
 		}
+
+	} else if (project !== true) {
+		// these pages don't work with vertical centre yet - some of them will break & others are never able to be centred as the content is always at least full height
+		const nonWorkingPages = ['imageSequence', 'mediaLesson', 'morphImages', 'thumbnailViewer', 'SictTimeline', 'perspectives', 'connectorMenu', 'links'];
+		if (x_currentPageXML.getAttribute("verticalAlign") != undefined && (nonWorkingPages == undefined || nonWorkingPages.indexOf(x_pageInfo[x_currentPage].type) < 0)) {
+			if (x_currentPageXML.getAttribute("verticalAlign") === "true") {
+				// turn on for this page
+				$x_pageHolder.addClass("x_verticalCentre");
+				if (x_currentPageXML.getAttribute("verticalAlignPanel") === "true") {
+					$x_pageHolder.addClass("x_verticalCentrePanel");
+				} else if (x_currentPageXML.getAttribute("verticalAlignPanel") === "false") {
+					$x_mainHolder.addClass("x_noVerticalCentrePanel");
+				}
+			} else if (x_currentPageXML.getAttribute("verticalAlign") === "false") {
+				// turn off for this page
+				$x_mainHolder.addClass("x_noVerticalCentre x_noVerticalCentrePanel");
+			}
+		} else if (nonWorkingPages != undefined && nonWorkingPages.indexOf(x_pageInfo[x_currentPage].type) >= 0) {
+			// always turn off for this type of page as it won't work properly
+			$x_mainHolder.addClass("x_noVerticalCentre x_noVerticalCentrePanel");
+		}
+	}
+}
+
+// function adds the scrolling class when required - this helps determine whether the x_verticalCentrePanel css should be applied or not
+// columns should only be centred if all columns fit on screen with no scrolling - otherwise align top
+function x_checkForScrolling() {
+	if ($x_pageHolder.prop('scrollHeight') > $x_pageHolder.innerHeight()) {
+		$x_pageHolder.addClass("x_scrolling");
+	} else {
+		$x_pageHolder.removeClass("x_scrolling");
 	}
 }
 
@@ -3147,14 +3164,7 @@ function x_changePageStep3() {
         // checks if size has changed since last load - if it has, call function in current page model which does anything needed to adjust for the change
         var prevSize = builtPage.data("size");
         if (prevSize[0] != $x_mainHolder.width() || prevSize[1] != $x_mainHolder.height()) {
-			if (typeof window[pt].sizeChanged === "function") window[pt].sizeChanged();
-
-            // calls function in any customHTML that's been loaded into page
-            if ($(".customHTMLHolder").length > 0) {
-                if (typeof customHTML.sizeChanged === "function") {
-                	customHTML.sizeChanged();
-                }
-            }
+			x_sizeChanged();
         }
 
 		// any custom header styles will be disabled if a custom theme (via accessibility options) is in use
@@ -3619,6 +3629,8 @@ function x_pageLoaded() {
 	}
 
 	XENITH.SIDEBAR.pageLoad(true);
+
+	x_checkForScrolling();
 }
 
 //convert picker color to #value
@@ -3876,23 +3888,26 @@ function x_updateCss2(updatePage) {
     }
 
     if (updatePage != false) {
-        try {
-            // calls function in current page model which does anything needed on size change
-            if (x_pageInfo[x_currentPage].type == "text") {
-                simpleText.sizeChanged(); // errors if you just call text.sizeChanged()
-            } else {
-                eval(x_pageInfo[x_currentPage].type).sizeChanged();
-            }
-        }
-        catch(e) {} // Catch error thrown when you call sizeChanged() on an unloaded model
-
-        // calls function in any customHTML that's been loaded into page
-        if ($(".customHTMLHolder").length > 0) {
-            try { customHTML.sizeChanged(); } catch(e) {};
-        }
+		x_sizeChanged();
     }
 
     $(".x_popupDialog").parent().detach();
+}
+
+function x_sizeChanged() {
+	// get short page type var
+	let pt = x_pageInfo[x_currentPage].type;
+	if (pt == "text") pt = 'simpleText'; // errors if you just call text.pageChanged()
+	if (typeof window[pt].sizeChanged === "function") window[pt].sizeChanged();
+
+	// calls function in any customHTML that's been loaded into page
+	if ($(".customHTMLHolder").length > 0) {
+		if (typeof customHTML.sizeChanged === "function") {
+			customHTML.sizeChanged();
+		}
+	}
+
+	x_checkForScrolling();
 }
 
 // functions open dialogs e.g. glossary, table of contents - just reattach if it's already loaded previously

modules/xerte/parent_templates/Nottingham/common_html5/css/mainStyles.css
@@ -1893,6 +1893,15 @@ button.resourceLink {
 	margin-top: 1em;
 }
 
+#x_mainHolder.x_verticalCentrePanel:not(.x_noVerticalCentre):not(.x_noVerticalCentrePanel) #x_pageHolder:not(.x_scrolling) .splitScreen_A,
+#x_mainHolder.x_verticalCentrePanel:not(.x_noVerticalCentre):not(.x_noVerticalCentrePanel) #x_pageHolder:not(.x_scrolling) .splitScreen_B,
+#x_pageHolder.x_verticalCentrePanel:not(.x_scrolling) .splitScreen_A,
+#x_pageHolder.x_verticalCentrePanel:not(.x_scrolling) .splitScreen_B {
+	display: flex;
+	justify-content: safe center;
+	flex-direction: column;
+}
+
 @media (max-width: 480px) {
 	/* split screen layout used in lots of page models - stack vertically on smaller screens */
 	.splitScreen {

modules/xerte/parent_templates/Nottingham/common_html5/js/popcorn/plugins/popcorn.mcq.js
@@ -244,7 +244,7 @@ optional: feedback page synch play enable
 				$target = $("#" + options.target);
 				var $optionText = options.name !== "" ? '<h4>' + options.name + '</h4>' + x_addLineBreaks(options.text) : x_addLineBreaks(options.text);
 				$target.hide();
-				debugger;
+				//debugger;
 				// Store the answers in a temporary array
 				var elements = [];
 				(options.childNodes).each(function(i) {

modules/xerte/parent_templates/Nottingham/models_html5/columnPage.css
@@ -41,4 +41,11 @@
 		margin-top:	10px;
 		border-top:	1px solid gray;
 	}
+
+	#x_mainHolder.x_verticalCentrePanel:not(.x_noVerticalCentre):not(.x_noVerticalCentrePanel) #x_pageHolder:not(.x_scrolling) #columnHolder .column,
+	#x_pageHolder.x_verticalCentrePanel:not(.x_scrolling) #columnHolder .column {
+		display: flex;
+		justify-content: safe center;
+		flex-direction: column;
+	}
 	

modules/xerte/parent_templates/Nottingham/models_html5/nav.css
@@ -95,7 +95,14 @@
 		gap: 1em;
 	}
 
-	
+	#x_mainHolder.x_verticalCentrePanel:not(.x_noVerticalCentre):not(.x_noVerticalCentrePanel) #x_pageHolder:not(.x_scrolling) #navHolder .column,
+	#x_pageHolder.x_verticalCentrePanel:not(.x_scrolling) #navHolder .column {
+		display: flex;
+		justify-content: safe center;
+		flex-direction: column;
+	}
+
+
 	/* __slides__ */
 	
 	#pageContents.slide .navChildTitle {

modules/xerte/parent_templates/Nottingham/models_html5/thumbnailViewer.html
@@ -246,7 +246,7 @@
 					"visibility": "hidden",
 					"display": "inherit"
 				});
-				debugger;
+
 				// add image
 				const $panelImg = $('<div id="panelImg"><img class="centre" alt="' + (thisData.getAttribute("tip") != undefined && thisData.getAttribute("tip") != "") + '" /></div>').appendTo($mainInfoHolder.find("#mainPanel"));
 				const $panelTxt = $('<div id="panelTxt" class="overlay"></div>');

src/Nottingham/wizards/en-GB/basic.xwd
@@ -18,7 +18,10 @@
 		<horizontalAlign label="Horizontal Alignment" optional="true" defaultValue="centre" options="Right,Centre,Left" data="right,centre,left" type="ComboBox"/>
 	</maxWidthGroup>
 
-  	<verticalAlign label="Centre Content Vertically" optional="true" type="CheckBox" defaultValue="true" tooltip="Centre the content vertically on the page. Can be used alongside the 'Maximum Width' optional properties to also centre content horizontally. A centre content vertically optional property is also available on each page where you can adjust these settings on a page by page basis."/>
+	<verticalAlignGroup type="group" label="Centre Content Vertically" optional="true" tooltip="Centre the content vertically on the page. Can be used alongside the 'Maximum Width' optional properties to also centre content horizontally. A centre content vertically optional property is also available on each page where you can adjust these settings on a page by page basis.">
+  		<verticalAlign label="Centre Content Vertically" optional="true" type="CheckBox" defaultValue="true" conditionTrigger="true"/>
+  		<verticalAlignPanel label="Centre Columns Individually" optional="true" type="CheckBox" defaultValue="true" condition="verticalAlign=='true'" tooltip="On pages that contain column layouts, e.g. page text next to a panel, centre each column vertically on the page"/>
+	</verticalAlignGroup>
 
 	<fixDisplay optional="true" type="TextInput" label="Fixed Display Size" defaultValue="800,600" condition="displayMode=='default'||(fixDisplay!=undefined&amp;&amp;fixDisplay!='')" tooltip="Sets the project dimensions (in pixels) when 'display mode' is set to 'fixed size'" />
 	

src/Nottingham/wizards/en-GB/basicPages.xwd
@@ -91,7 +91,10 @@
 		<horizontalAlign label="Horizontal Alignment" optional="true" defaultValue="centre" options="Right,Centre,Left" data="right,centre,left" type="ComboBox" condition="maxWidthToggle=='true'"/>
 	</maxWidthGroup>
 
-	<verticalAlign label="Centre Content Vertically" optional="true" type="CheckBox" defaultValue="true" tooltip="Centre the content vertically on the page. This will override the setting at project level."/>
+	<verticalAlignGroup type="group" label="Centre Content Vertically" optional="true" tooltip="Centre the content vertically on the page. This will override the setting at project level.">
+		<verticalAlign label="Centre Content Vertically" optional="true" type="CheckBox" defaultValue="true" conditionTrigger="true" />
+		<verticalAlignPanel label="Centre Columns Individually" optional="true" type="CheckBox" defaultValue="true" condition="verticalAlign=='true'" tooltip="On pages that contain column layouts, e.g. page text next to a panel, centre each column vertically on the page"/>
+	</verticalAlignGroup>
 
 	<pageTimer type="group" label="Page Timer" optional="true" tooltip="Add a timer to the bottom of the page, counting down to zero">
 		<showTimer label="Show Timer" optional="true" type="CheckBox" defaultValue="true" conditionTrigger="true"/>

commit d7eefdfd2816084d9f6ce2c48fee779a0e8646bc

Catch errors caused by a previous commit https://github.com/thexerteproject/xerteonlinetoolkits/commit/c64e2bf16ab5b3bd687939cc267d495e36722c8d

modules/xerte/parent_templates/Nottingham/common_html5/js/xenith.js
@@ -3176,7 +3176,7 @@ function x_changePageStep3() {
         // checks if size has changed since last load - if it has, call function in current page model which does anything needed to adjust for the change
         var prevSize = builtPage.data("size");
         if (prevSize[0] != $x_mainHolder.width() || prevSize[1] != $x_mainHolder.height()) {
-			x_sizeChanged();
+			x_sizeChanged(false, true);
         }
 
 		// any custom header styles will be disabled if a custom theme (via accessibility options) is in use
@@ -3885,7 +3885,6 @@ function x_updateCss(updatePage, updateSidebar) {
 			
 		}
 	}
-
 	x_updateCss2(updatePage);
 }
 
@@ -3907,16 +3906,22 @@ function x_updateCss2(updatePage) {
     $(".x_popupDialog").parent().detach();
 }
 
-function x_sizeChanged() {
-	// get short page type var
-	let pt = x_pageInfo[x_currentPage].type;
-	if (pt == "text") pt = 'simpleText'; // errors if you just call text.pageChanged()
-	if (typeof window[pt].sizeChanged === "function") window[pt].sizeChanged();
+function x_sizeChanged(page, custom) {
+	try {
+		// get short page type var
+		if (page !== false) {
+			let pt = x_pageInfo[x_currentPage].type;
+			if (pt == "text") pt = 'simpleText'; // errors if you just call text.pageChanged()
+			if (typeof window[pt].sizeChanged === "function") window[pt].sizeChanged();
+		}
+	} catch(e) {} // Catch error thrown when you call sizeChanged() on an unloaded model
 
-	// calls function in any customHTML that's been loaded into page
-	if ($(".customHTMLHolder").length > 0) {
-		if (typeof customHTML.sizeChanged === "function") {
-			customHTML.sizeChanged();
+	if (custom !== false) {
+		// calls function in any customHTML that's been loaded into page
+		if ($(".customHTMLHolder").length > 0) {
+			if (typeof customHTML.sizeChanged === "function") {
+				customHTML.sizeChanged();
+			}
 		}
 	}
 