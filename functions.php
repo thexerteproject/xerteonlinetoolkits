f7560df4fc9d20b92ee6ce62a604415a4ba63a45
Tom Reijnders <tom.reijnders@dlearning.nl>
2026-05-12 21:25:53 +0200

    Fix vulnerabilities (XSS and path traversal)

--- functions.php
+++ functions.php
@@ -366,6 +366,20 @@ function x_clean_input($input, $expected_type = null, $specialcharsflags = ENT_Q
             }
             return $input; // do not modify booleans
         }
+        else if ($expected_type == 'email') {
+            if (!filter_var($sanitized, FILTER_VALIDATE_EMAIL)) {
+                die("Expected email address, got " . htmlentities($sanitized, $specialcharsflags));
+            }
+        }
+        else if ($expected_type == 'url') {
+            if (!filter_var($sanitized, FILTER_VALIDATE_URL)) {
+                die("Expected URL, got " . htmlentities($sanitized, $specialcharsflags));
+            }
+         }
+        else
+        {
+            die("Invalid expected type '$expected_type' specified in x_clean_input: " . htmlentities($expected_type, $specialcharsflags));
+        }
     }
     return $sanitized;
 }
